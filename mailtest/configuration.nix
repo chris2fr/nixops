fd5fdd99507563ad3b254ad05392483ff16fe977
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-15 11:55:46 +0200

    Test Build

--- mailtest/configuration.nix
+++ mailtest/configuration.nix
@@ -1,17 +1,46 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+let 
+  mannchriRsaPublic = (builtins.readFile ./.sercrets.mannchri-rsa.pub);
+in
 {
+  nix.settings.experimental-features = "nix-command flakes";
   imports = [
     ./vpsadminos.nix
+    #<home-manager/nixos>
   ];
 
   environment.systemPackages = with pkgs; [
-    vim
+    ((vim_configurable.override {  }).customize{
+      name = "vim";
+      vimrcConfig.customRC = ''
+        " your custom vimrc
+        set mouse=a
+        set nocompatible
+        colo torte
+        syntax on
+        set tabstop     =2
+        set softtabstop =2
+        set shiftwidth  =2
+        set expandtab
+        set autoindent
+        set smartindent
+        " ...
+      '';
+      }
+    )
+    curl
+    wget
+    lynx
+    git
+    tmux
+    bat
+    zlib
+    dig
+    lzlib
   ];
 
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
-  #users.extraUsers.root.openssh.authorizedKeys.keys =
-  #  [ "..." ];
 
   systemd.extraConfig = ''
     DefaultTimeoutStartSec=900s
@@ -20,4 +49,12 @@
   time.timeZone = "Europe/Amsterdam";
 
   system.stateVersion = "23.05";
+
+  environment.sessionVariables = rec {
+    EDITOR="vim";
+  };
+#  security.acme = {
+#    acceptTerms = true;
+#    defaults.email = "contact@lesgrandsvoisins.com";
+#  };
 }
