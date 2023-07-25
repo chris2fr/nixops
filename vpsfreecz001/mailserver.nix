4b0eb3e760984dd9aa8a79ba683813de56ba5a1d
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-25 07:41:25 +0200

    Adding Fail2Ban

--- vpsfreecz001/mailserver.nix
+++ vpsfreecz001/mailserver.nix
@@ -447,28 +447,28 @@ services.fail2ban = {
         bantime  = 600
         maxretry = 5
       '';
-      postfix = ''
-        port     = smtp,465,submission
-        logpath  = /var/log/fail2ban.log
-        backend  = auto
-        enabled  = true
-        mode     = more
-      '';
-      dovecot = ''
-        port     = smtp,465,submission
-        logpath  = /var/log/fail2ban.log
-        backend  = auto
-        enabled  = true
-        mode     = more
-      '';
-      postfix-sasl = ''
-        filter   = postfix[mode=auth]
-        port     = smtp,465,submission,imap,imaps,pop3,pop3s
-        logpath  = /var/log/fail2ban.log
-        backend  = auto
-        enabled  = true
-        mode     = more
-      '';
+      # postfix = ''
+      #   port     = smtp,465,submission
+      #   logpath  = /var/log/fail2ban.log
+      #   backend  = auto
+      #   enabled  = true
+      #   mode     = more
+      # '';
+      # dovecot = ''
+      #   port     = smtp,465,submission
+      #   logpath  = /var/log/fail2ban.log
+      #   backend  = auto
+      #   enabled  = true
+      #   mode     = more
+      # '';
+      # postfix-sasl = ''
+      #   filter   = postfix[mode=auth]
+      #   port     = smtp,465,submission,imap,imaps,pop3,pop3s
+      #   logpath  = /var/log/fail2ban.log
+      #   backend  = auto
+      #   enabled  = true
+      #   mode     = more
+      # '';
     };
   };
 
