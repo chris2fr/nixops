8d53ee47e34121f96b5ad51085a2df311522bd20
Chris Mann <chris@lesgrandsvoisins.com>
2023-07-26 16:19:57 +0200

    Added meet, app, auth, biz, forum, mail, wiki to *.lesgrandsvoisins.com

--- vpsfreecz001/mailserver.nix
+++ vpsfreecz001/mailserver.nix
@@ -24,108 +24,61 @@ in
     isNormalUser = true;
   };
 ################################################################################################################
-  services.httpd.virtualHosts."mail.lesgrandsvoisins.com" = {
+  services.httpd.virtualHosts."lesgv.com" = {
+    serverAliases = ["mail.resdigita.org" "www.lesgv.com" "lesgv.org" "resdigita.org" "www.resdigita.org" "resdigita.com" "www.lesgv.org" "www.resdigita.com"];
     enableACME = true;
     forceSSL = true;
     documentRoot =  "/var/www/SOGo";
-    extraConfig = ''
-      Alias /SOGo.woa/WebServerResources/js/theme.js /var/www/SOGo/WebServerResources/theme.js
-      Alias /.woa/WebServerResources/ /var/www/SOGo/WebServerResources/
-      Alias /SOGo.woa/WebServerResources/ /var/www/SOGo/WebServerResources/
-      Alias /SOGo/WebServerResources/ /var/www/SOGo/WebServerResources/
-      Alias /WebServerResources/ /var/www/SOGo/WebServerResources/
-      <Directory /var/www/SOGo/WebServerResources/>
-        AllowOverride none
-        Require all granted
-        <IfModule expires_module>
-          ExpiresActive On
-          ExpiresDefault "access plus 1 year"
-        </IfModule>
-      </Directory>
-      ProxyPass /.well-known !
-      ProxyPass /.woa/WebServerResources/ !
-      ProxyPass /SOGo.woa/WebServerResources/  !
-      ProxyPass /SOGo/WebServerResources/  !
-      ProxyPass /WebServerResources/  !
-      ProxyPass /SOGo.woa/ !
-      ProxyPass / http://[::1]:20000/SOGo/ retry=0
-      ProxyRequests Off
-      SetEnv proxy-nokeepalive 1
-      ProxyPreserveHost On
-      CacheDisable /
-      <Proxy http://127.0.0.1:20000/SOGo >
-        SetEnvIf Host (.*) custom_host=$1
-        RequestHeader set "x-webobjects-server-name" "%{custom_host}e"
-        RequestHeader set "x-webobjects-server-url" "https://%{custom_host}e"
-        RequestHeader set "x-webobjects-server-port" "443"
-        # When using proxy-side autentication, you need to uncomment and
-        ## adjust the following line:
-        RequestHeader unset "x-webobjects-remote-user"
-        #  RequestHeader set "x-webobjects-remote-user" "%{REMOTE_USER}e" env=REMOTE_USER
-        RequestHeader set "x-webobjects-server-protocol" "HTTP/1.0"
-        AddDefaultCharset UTF-8
-        Order allow,deny
-        Allow from all
-      </Proxy>
-    '';
-  };
-  services.httpd.virtualHosts."app.lesgrandsvoisins.com" = {
-    serverAliases = ["mail.resdigita.org" "www.lesgv.com" "lesgv.org" "resdigita.org" "www.resdigita.org" "resdigita.com" "www.lesgv.org" "www.resdigita.com" "lesgv.com];
-    enableACME = true;
-    forceSSL = true;
-    # documentRoot =  "/var/www/SOGo";
     globalRedirect = "https://mail.resdigita.com/";
   };
   services.httpd.virtualHosts."mail.resdigita.com" = {
-    serverAliases = ["gvoisin.com" "www.gvoisin.com" "mail.gvoisin.com" "gvoisin.org" "www.gvoisin.org" "gvoisins.org" "www.gvoisins.org" "gvoisins.com" "www.gvoisins.com"];
+    serverAliases = ["gvoisin.com" "www.gvoisin.com" "mail.gvoisin.com" "gvoisin.org" "www.gvoisin.org" "gvoisins.org" "www.gvoisins.org" "gvoisins.com" "www.gvoisins.com" ];
     enableACME = true;
     forceSSL = true;
     documentRoot =  "/var/www/SOGo";
     extraConfig = ''
-      Alias /SOGo.woa/WebServerResources/js/theme.js /var/www/SOGo/WebServerResources/theme.js
-      Alias /.woa/WebServerResources/ /var/www/SOGo/WebServerResources/
-      Alias /SOGo.woa/WebServerResources/ /var/www/SOGo/WebServerResources/
-      Alias /SOGo/WebServerResources/ /var/www/SOGo/WebServerResources/
-      Alias /WebServerResources/ /var/www/SOGo/WebServerResources/
-      <Directory /var/www/SOGo/WebServerResources/>
-        AllowOverride none
-        Require all granted
-        <IfModule expires_module>
-          ExpiresActive On
-          ExpiresDefault "access plus 1 year"
-        </IfModule>
-      </Directory>
-      ProxyPass /.well-known !
-      ProxyPass /.woa/WebServerResources/ !
-      ProxyPass /SOGo.woa/WebServerResources/  !
-      ProxyPass /SOGo/WebServerResources/  !
-      ProxyPass /WebServerResources/  !
-      ProxyPass /SOGo/ http://[::1]:20000/SOGo/ retry=0
-      ProxyPass /SOGo http://[::1]:20000/SOGo retry=0
-      ProxyPass / http://localhost:9991/ retry=0
-      ProxyRequests Off
-      SetEnv proxy-nokeepalive 1
-      ProxyPreserveHost On
-      CacheDisable /
-      <Proxy http://127.0.0.1:20000/SOGo >
-        ## HERE 
-        ## HERE 
-        ## HERE 
-        RequestHeader set "x-webobjects-server-port" "443"
-        # When using proxy-side autentication, you need to uncomment and
-        ## adjust the following line:
-        RequestHeader unset "x-webobjects-remote-user"
-        # HERE2
-        RequestHeader set "x-webobjects-server-protocol" "HTTP/1.0"
-        AddDefaultCharset UTF-8
-        Order allow,deny
-        Allow from all
-      </Proxy>
+    Alias /SOGo.woa/WebServerResources/js/theme.js /var/www/SOGo/WebServerResources/theme.js
+    Alias /.woa/WebServerResources/ /var/www/SOGo/WebServerResources/
+    Alias /SOGo.woa/WebServerResources/ /var/www/SOGo/WebServerResources/
+    Alias /SOGo/WebServerResources/ /var/www/SOGo/WebServerResources/
+    Alias /WebServerResources/ /var/www/SOGo/WebServerResources/
+    <Directory /var/www/SOGo/WebServerResources/>
+      AllowOverride none
+      Require all granted
+      <IfModule expires_module>
+        ExpiresActive On
+        ExpiresDefault "access plus 1 year"
+      </IfModule>
+    </Directory>
+    ProxyPass /.well-known !
+    ProxyPass /.woa/WebServerResources/ !
+    ProxyPass /SOGo.woa/WebServerResources/  !
+    ProxyPass /SOGo/WebServerResources/  !
+    ProxyPass /WebServerResources/  !
+    ProxyPass /SOGo/ http://[::1]:20000/SOGo/ retry=0
+    ProxyPass /SOGo http://[::1]:20000/SOGo retry=0
+    ProxyPass / http://localhost:9991/ retry=0
+    ProxyRequests Off
+    SetEnv proxy-nokeepalive 1
+    ProxyPreserveHost On
+    CacheDisable /
+    <Proxy http://127.0.0.1:20000/SOGo >
+      SetEnvIf Host (.*) custom_host=$1
+      RequestHeader set "x-webobjects-server-name" "%{custom_host}e"
+      RequestHeader set "x-webobjects-server-url" "https://%{custom_host}e"
+      RequestHeader set "x-webobjects-server-port" "443"
+      # When using proxy-side autentication, you need to uncomment and
+      ## adjust the following line:
+      RequestHeader unset "x-webobjects-remote-user"
+      #  RequestHeader set "x-webobjects-remote-user" "%{REMOTE_USER}e" env=REMOTE_USER
+      RequestHeader set "x-webobjects-server-protocol" "HTTP/1.0"
+      AddDefaultCharset UTF-8
+      Order allow,deny
+      Allow from all
+    </Proxy>
     '';
   };
 ###################################################################################################################################
-
-
   services.openldap = {
     enable = true;
     # urlList = [ "ldap:///" ];
