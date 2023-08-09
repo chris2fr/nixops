fd1ede8598f6ff615dfe6066671d148568453ffe
Chris Mann <chris@lesgrandsvoisins.com>
2023-08-09 08:08:23 +0200

    This is a more sensible way of handing home-manager

--- hetzner/configuration.nix
+++ hetzner/configuration.nix
@@ -5,6 +5,8 @@
 { config, pkgs, ... }:
 let
   mannchriRsaPublic = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuBWybYSoR6wyd1EG5YnHPaMKE3RQufrK7ycej7avw3Ug8w8Ppx2BgRGNR6EamJUPnHEHfN7ZZCKbrAnuP3ar8mKD7wqB2MxVqhSWvElkwwurlijgKiegYcdDXP0JjypzC7M73Cus3sZT+LgiUp97d6p3fYYOIG7cx19TEKfNzr1zHPeTYPAt5a1Kkb663gCWEfSNuRjD2OKwueeNebbNN/OzFSZMzjT7wBbxLb33QnpW05nXlLhwpfmZ/CVDNCsjVD1+NXWWmQtpRCzETL6uOgirhbXYW8UyihsnvNX8acMSYTT9AA3jpJRrUEMum2VizCkKh7bz87x7gsdA4wF0/w== rsa-key-20220407";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+
 in
 {
   imports =
@@ -17,7 +19,7 @@ in
 #    ./users.nix
     ./wagtail.nix
     ./common.nix # Des configurations communes pratiques
-    <home-manager/nixos>
+    (import "${home-manager}/nixos")
     ];
 
   # Use the systemd-boot EFI boot loader.
