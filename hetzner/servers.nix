07c03544b2702dc25aed661cd406a8c7106d749b
Chris Mann <chris@lesgrandsvoisins.com>
2023-08-09 12:39:03 +0200

    Unfree

--- hetzner/servers.nix
+++ hetzner/servers.nix
@@ -8,6 +8,7 @@ in
   imports = [
     (import "${home-manager}/nixos")
   ];
+  nixpkgs.config.allowUnfree = true;
   users.users.aaa = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [ mannchriRsaPublic ];
