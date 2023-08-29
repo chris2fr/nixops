8b78177b2bcfb4e82d408f7bbda31ecf61e68eff
Chris Mann <chris@lesgrandsvoisins.com>
2023-08-29 09:36:46 +0200

    Migrating to Stage

--- stage/configuration.nix
+++ stage/configuration.nix
@@ -150,31 +150,7 @@ in
   virtualisation.lxd.enable = true;
   virtualisation.lxc.enable = true;
   virtualisation.lxc.lxcfs.enable = true;
-  ## Adding httpd
-  services.httpd.enable = true;
-  services.httpd.enablePHP = true;
-  services.httpd.phpOptions = ''
-    upload_max_filesize = 128M
-    post_max_size = 256M
-    extension=imagick.so
-  '';
-  services.httpd.virtualHosts."vpsfreecz003.lesgrandsvoisins.com" = {
-    serverAliases = [
-      "ghh.villagevoisin.com"
-    ];
-    enableACME = true;
-    forceSSL = true;
-    documentRoot = "/var/www/ghh";
-    extraConfig = ''
-      <Directory />
-        DirectoryIndex index.php
-        Require all granted
-      </Directory>
-      '';
-  };
-  services.mysql.package = pkgs.mariadb;
-  services.mysql.enable = true;
-
+ 
 
 }
 
--- stage/wordpress.nix
+++ stage/wordpress.nix
@@ -4,7 +4,33 @@ in
 {
   environment.systemPackages = with pkgs; [
     mariadb
-    php81Extensions.imagick
-
+    php82
+    php82Extensions.imagick
   ];
+   ## Adding httpd
+  services.httpd.enable = true; 
+  services.httpd.enablePHP = true;
+  services.httpd.phpPackage = pkgs.php82;
+  services.httpd.phpOptions = ''
+    upload_max_filesize = 128M
+    post_max_size = 256M
+    extension=imagick.so
+  '';
+  services.httpd.virtualHosts."vpsfreecz003.lesgrandsvoisins.com" = {
+    serverAliases = [
+      "ghh.villagevoisin.com"
+    ];
+    enableACME = true;
+    forceSSL = true;
+    documentRoot = "/var/www/ghh";
+    extraConfig = ''
+      <Directory />
+        DirectoryIndex index.php
+        Require all granted
+      </Directory>
+      '';
+  };
+  services.mysql.package = pkgs.mariadb;
+  services.mysql.enable = true;
+
 }
