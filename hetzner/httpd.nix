6be730263f8f7c784ec98be13b2a62f46e70eb4f
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-01 17:46:35 +0100

    Reverse Proxy Syntax

--- hetzner/httpd.nix
+++ hetzner/httpd.nix
@@ -62,13 +62,17 @@ in
     enableACME = true;
     forceSSL = true;
     extraConfig = ''
-        proxy_pass http://10.245.101.35:9000;
-        proxy_http_version 1.1;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Host $host;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection $connection_upgrade_keepalive;
+        ProxyPass /  http://10.245.101.35:9000/
+        # proxy_http_version 1.1;
+        RequestHeader set X-Forwarded-Proto "https"
+        RequestHeader set X-Forwarded-Port "443"
+        #RequestHeader set X-Forwarded-For "$proxy_add_x_forwarded_for
+        #RequestHeader set Host $host
+        #RequestHeader set Upgrade $http_upgrade
+        #RequestHeader set Connection $connection_upgrade_keepalive
+        ProxyPreserveHost On
+        ProxyVia On
+        ProxyAddHeaders On
     '';
   };
   services.httpd.virtualHosts."blog.gvois.in" = {
