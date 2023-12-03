f17c01d064b16abf2b541f49851894aa18770713
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-03 14:41:54 +0100

    Giving access to host networks

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -13,11 +13,11 @@ in
   containers.wagtail = {
     autoStart = true;
     # privateNetwork = true;
-    hostBridge = "br0";
-    hostAddress = "192.168.100.10";
-    localAddress = "192.168.100.11";
-    hostAddress6 = "fc00::1";
-    localAddress6 = "fc00::2";
+    # hostBridge = "br0";
+    # hostAddress = "192.168.100.10";
+    # localAddress = "192.168.100.11";
+    # hostAddress6 = "fc00::1";
+    # localAddress6 = "fc00::2";
 
     config = { config, pkgs, ... }: {
       nix.settings.experimental-features = "nix-command flakes";
