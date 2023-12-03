51424811ba3123568f4238e1eec17547ffc0f130
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-03 13:46:01 +0100

    Containers to test

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -1,33 +1,38 @@
-networking.nat = {
-  enable = true;
-  internalInterfaces = ["ve-+"];
-  externalInterface = "ens3";
-  # Lazy IPv6 connectivity for the container
-  enableIPv6 = true;
-};
-
-containers.wagtail = {
-  autoStart = true;
-  privateNetwork = true;
-  hostAddress = "192.168.100.10";
-  localAddress = "192.168.100.11";
-  hostAddress6 = "fc00::1";
-  localAddress6 = "fc00::2";
-  config = { config, pkgs, ... }: {
-
-
-    system.stateVersion = "23.05";
-
-    networking = {
-      firewall = {
-        enable = true;
-        allowedTCPPorts = [ 80 443 ];
-      };
-      # Use systemd-resolved inside the container
-      useHostResolvConf = mkForce false;
-    };
-    
-    services.resolved.enable = true;
-
+{ config, pkgs, lib, ... }:
+let
+in
+{
+  networking.nat = {
+    enable = true;
+    internalInterfaces = ["ve-+"];
+    externalInterface = "ens3";
+    # Lazy IPv6 connectivity for the container
+    enableIPv6 = true;
   };
-};
+
+  containers.wagtail = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.100.10";
+    localAddress = "192.168.100.11";
+    hostAddress6 = "fc00::1";
+    localAddress6 = "fc00::2";
+    config = { config, pkgs, ... }: {
+
+
+      system.stateVersion = "23.05";
+
+      networking = {
+        firewall = {
+          enable = true;
+          allowedTCPPorts = [ 80 443 ];
+        };
+        # Use systemd-resolved inside the container
+        useHostResolvConf = mkForce false;
+      };
+      
+      services.resolved.enable = true;
+
+    };
+  };
+}
