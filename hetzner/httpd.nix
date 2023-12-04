53a9e624bb4a38056dba655ad0c773cca839528b
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-04 11:53:53 +0100

    Hold DAV

--- hetzner/httpd.nix
+++ hetzner/httpd.nix
@@ -109,13 +109,13 @@ in
     forceSSL = true;
     documentRoot = "/var/www/dav/";
     extraConfig = ''
-      DavLockDB /tmp/DavLock
-      AuthLDAPSearchAsUser on
-      AuthLDAPURL "ldap:///ou=users,dc=resdigita,dc=org?cn?sub"
-      <Location "/chris">
-      #Require valid-user
-      Require ldap-dn cn=chris@lesgrandsvoisins.com,ou=users,dc=resdigita,dc=org
-      </Location>
+      # DavLockDB /tmp/DavLock
+      # AuthLDAPSearchAsUser on
+      # AuthLDAPURL "ldap:///ou=users,dc=resdigita,dc=org?cn?sub"
+      # <Location "/chris">
+      # #Require valid-user
+      # Require ldap-dn cn=chris@lesgrandsvoisins.com,ou=users,dc=resdigita,dc=org
+      # </Location>
 
     '';
   };
