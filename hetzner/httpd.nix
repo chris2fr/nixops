8ee47a02125309e7b100c4dfc8f6a82b6222b98b
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-04 11:31:08 +0100

    redirect vanity strange

--- hetzner/httpd.nix
+++ hetzner/httpd.nix
@@ -128,7 +128,7 @@ in
         OIDCProviderMetadataURL https://authentik.lesgrandsvoisins.com/application/o/dav/.well-known/openid-configuration
         OIDCClientID V7p2o3hX6Im6crzdExLI1lb81zMJEjDO3mO3rNBk
         OIDCClientSecret Qgi9BFz7UOzwsJUAtN5Pa28sUL4oyrbkv2gvpsELMUgksPoLReS2eu9aHqJezyyoquJV02IX0UFPB8cvIB8uC9OW42MC4q8qswVeuM6aOUSvEXas1lQKnwAxad5sWrXc
-        OIDCRedirectURI https://dav.desgv.com/secure/redirect_uri
+        OIDCRedirectURI https://dav.desgv.com/chris/redirect_uri
         OIDCCryptoPassphrase JoWT5Mz1DIzsgI3MT2GH82aA6Xamp2ni
         <Location "/chris">
           AuthType openid-connect
