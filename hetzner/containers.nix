aaa5cf53cd6ff315c553e1433640e1ee8b252970
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-06 13:35:09 +0100

    No pip needed

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -202,7 +202,6 @@ in
             python311Packages.pillow
             python311Packages.gunicorn
             python311Packages.pip
-            python311Packages.python-ldap
             libjpeg
             zlib
             libtiff
