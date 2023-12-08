b3591dab67efb08aabfa91b5e06e6a3a43f243b9
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-08 07:54:55 +0100

    auth.lesgrandsvoisins.com:8888 and 8443

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -7,7 +7,12 @@ in
     defaultListenPort = 8443;
     defaultHTTPListenPort = 8888;
     defaultListen = [{ addr = "0.0.0.0"; } { addr = "[::0]"; }];
-    
-  }
+
+  };
+  virtualHosts."auth.lesgrandsvoisins.com" = {
+    sslTrustedCertificate = /var/lib/acme/auth.lesgrandsvoisins.com/fullchain.pem;
+    sslCertificateKey = /var/lib/acme/auth.lesgrandsvoisins.com/key.pem;
+    sslCertificateChainFile = /var/lib/acme/auth.lesgrandsvoisins.com/chain.pem;
+  };
 
 }
