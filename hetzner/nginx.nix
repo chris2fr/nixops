97c67653cdb11ffb950640d389705808b3c80b65
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-08 09:32:52 +0100

    Nginx corrected

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -4,15 +4,14 @@ in
 { 
   services.nginx = {
     enable = true;
-    defaultListenPort = 8443;
+    defaultSSLListenPort = 8443;
     defaultHTTPListenPort = 8888;
     defaultListen = [{ addr = "0.0.0.0"; } { addr = "[::0]"; }];
-
-  };
-  virtualHosts."auth.lesgrandsvoisins.com" = {
-    sslTrustedCertificate = /var/lib/acme/auth.lesgrandsvoisins.com/fullchain.pem;
-    sslCertificateKey = /var/lib/acme/auth.lesgrandsvoisins.com/key.pem;
-    sslCertificateChainFile = /var/lib/acme/auth.lesgrandsvoisins.com/chain.pem;
+    virtualHosts."auth.lesgrandsvoisins.com" = {
+      sslTrustedCertificate = /var/lib/acme/auth.lesgrandsvoisins.com/fullchain.pem;
+      sslCertificateKey = /var/lib/acme/auth.lesgrandsvoisins.com/key.pem;
+      sslCertificateChainFile = /var/lib/acme/auth.lesgrandsvoisins.com/chain.pem;
+    };
   };
 
 }
