4d8a12be97a478e93992585ab56b9576ae6f98cc
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-11 15:44:12 +0100

    Fixed

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -52,6 +52,9 @@ in
         proxy_set_header Host $host;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection $connection_upgrade_keepalive;
+        
+        proxy_buffering off;
+        tcp_nodelay on;
       '';
     };
   };
