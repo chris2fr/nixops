2361ebfd035493d215969d8d71d9efcc5da37a7b
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-11 18:01:02 +0100

    Nginx problem

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -53,10 +53,10 @@ in
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection $connection_upgrade_keepalive;
         
-        proxy_buffering off;
-        tcp_nodelay on;
+
       '';
-    };
+        # proxy_buffering off;
+        # tcp_nodelay on;    };
   };
 
 }
