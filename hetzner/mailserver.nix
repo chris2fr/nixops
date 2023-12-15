2fe095f60c2bf612baa8ce6d8776dddb2bf273c2
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-15 21:56:55 +0100

    ][

--- hetzner/mailserver.nix
+++ hetzner/mailserver.nix
@@ -204,7 +204,7 @@ in
         $config['oauth_scope'] = "openid dovecotprofile";
         $config['oauth_auth_parameters'] = [];
         $config['oauth_identity_fields'] = ['email'];
-        $config['session_domain'] = ['mail.lesgrandsvoisins.com'];
+        $config['session_domain'] = 'mail.lesgrandsvoisins.com';
         $config['generic_message_footer_html'] = '<a href="https://www.lesgrandsvoisins.com">Les Grands Voisins .com comme communautés</a>';
         $config['session_samesite'] = "Lax";
         $config['support_url'] = 'https://www.lesgrandsvoisins.com';
