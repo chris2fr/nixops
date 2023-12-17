b9d578c546e89b2610a786c28aca8d9c869d7f68
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-17 12:01:41 +0100

    users.users.wwwrun.isSystemUser = true;

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -22,9 +22,10 @@ let
     '';
 in
 { 
+  users.users.wwwrun.isSystemUser = true;
   services.nginx = {
     user = "wwwrun";
-    group = "wwwrun";
+    # group = "wwwrun";
     enable = true;
     # defaultListen = [
     #     {addr = "[2a01:4f8:241:4faa::100]"; port=443; ssl=true;}
