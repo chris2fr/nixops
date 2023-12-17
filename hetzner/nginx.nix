56839e0b6705a8414020df69922b9ad9306e4cc9
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-17 10:41:19 +0100

    forceSSL

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -34,20 +34,21 @@ in
       sslCertificateKey = "/var/lib/acme/hetzner005.lesgrandsvoisins.com/key.pem";
       sslTrustedCertificate = "/var/lib/acme/hetzner005.lesgrandsvoisins.com/fullchain.pem";
       listenAddresses = [ "0.0.0.0" "116.202.236.241" "[::]" "[::1]"];
+      forceSSL = true;
       listen = [{addr="0.0.0.0";port = 8443; ssl=true;} {addr="[::]";port = 8443; ssl=true;}{addr="116.202.236.241";port = 8443; ssl=true;} {addr="[::1]";port = 8443; ssl=true;}];
-      locations."/" = {
-        proxyPass = "https://www.lesgrandsvoisins.com";
-        extraConfig = ''
-          # proxy_redirect off;
-          proxy_set_header Host $host:$server_port;
-          # proxy_set_header Host $http_host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_pass_request_headers      on;
-          # proxy_redirect default;
-          # proxy_redirect ~^(https?://[^:]+):\d+(?<relpath>/.+)$ https://www.lesgrandsvoisins.com$relpath;
+      # locations."/" = {
+      #   proxyPass = "https://www.lesgrandsvoisins.com";
+      #   extraConfig = ''
+      #     # proxy_redirect off;
+      #     proxy_set_header Host $host:$server_port;
+      #     # proxy_set_header Host $http_host;
+      #     proxy_set_header X-Real-IP $remote_addr;
+      #     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      #     proxy_pass_request_headers      on;
+      #     # proxy_redirect default;
+      #     # proxy_redirect ~^(https?://[^:]+):\d+(?<relpath>/.+)$ https://www.lesgrandsvoisins.com$relpath;
 
-        '';
+      #   '';
       };
     };
     recommendedProxySettings = true;
