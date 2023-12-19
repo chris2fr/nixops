ebfbaf9dda7f68d3a52c92df196d763ece171392
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-19 14:45:11 +0100

    Setting up a new virtual machine

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -163,6 +163,54 @@ in
 
   # };
 
+  containers.crabfit = {
+    autoStart = true;
+    config = { config, pkgs, ... }: {
+    networking.firewall.allowedTCPPorts = [ 22 25 80 443 143 587 993 995 636 8443 9443 ];
+    nix.settings.experimental-features = "nix-command flakes";
+      time.timeZone = "Europe/Amsterdam";
+      system.stateVersion = "23.11";
+      environment.sessionVariables = rec {
+        NEXT_PUBLIC_API_URL = "apicrabfit.resdigita.com";
+        FRONTEND_URL =	"https: //crabfit.resdigita.com";
+      };
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+          curl
+          wget
+          lynx
+          dig    
+          tmux
+          bat
+          cowsay
+          lzlib
+          killall
+          pwgen
+          gettext
+          sqlite
+          nodePackages.vercel
+          flyctl
+      ];
+    };
+  };
+
   containers.wagtail = {
     
     
