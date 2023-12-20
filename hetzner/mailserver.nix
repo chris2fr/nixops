cad57a329a91ceadbdef22e4d16e2f62873032f5
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-21 00:38:27 +0100

    user roundcube

--- hetzner/mailserver.nix
+++ hetzner/mailserver.nix
@@ -289,8 +289,6 @@ in
   users.users.nginx.extraGroups = ["wwwrun"];
 
     services.phpfpm.pools."roundcube" = {
-    user = lib.mkForce "wwwrun";
-    group = lib.mkForce "wwwrun";
     settings = {
       "listen.owner" = lib.mkForce "wwwrun";
       "listen.group" = lib.mkForce "wwwrun";
