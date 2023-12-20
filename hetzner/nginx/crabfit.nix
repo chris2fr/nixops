bb5b1e3216803b220067b4938b68eafe07bcd00d
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-20 23:12:55 +0100

    forceSSL = true;

--- hetzner/nginx/crabfit.nix
+++ hetzner/nginx/crabfit.nix
@@ -5,6 +5,7 @@ in
   services.nginx.virtualHosts = {
     "crabfit.resdigita.com" = {
       enableACME = true;
+      forceSSL = true;
       locations."/" = {
         proxyPass = "http://localhost:3080/";
       };
@@ -14,6 +15,7 @@ in
       forceSSL = true;
       serverAliases = ["crabfit.desgrandsvoisins.com"];
       globalRedirect = "crabfit.resdigita.com";
+      # rencontre-avec-bgeparif-sviatlana-et-dea-ladapt-visio-243095
     };
     "apicrabfit.resdigita.com" = {
       enableACME = true;
