6267f9f0edbeefe326ddb5240b52d3685c7d44e3
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-24 08:07:07 +0100

    lib.

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -266,7 +266,7 @@ in
       };
       # Use systemd-resolved inside the container
       # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
-      useHostResolvConf = mkForce false;
+      useHostResolvConf = lib.mkForce false;
     };
     
     services.resolved.enable = true;
