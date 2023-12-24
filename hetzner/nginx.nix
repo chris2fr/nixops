aee1b984a71e998080de243c3757b0db77b8c83f
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-24 21:29:43 +0100

    extra headers

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -75,8 +75,15 @@ in
         "filebrowser.resdigita.com" = {
           enableACME = true;
           forceSSL = true;
-          locations."/".proxyPass = "https://filebrowser.resdigita.com:8443";
-          # locations."/".proxyPass = "http://localhost:8334";
+          locations."/" = {
+            proxyPass = "https://filebrowser.resdigita.com:8443";
+            # locations."/".proxyPass = "http://localhost:8334";
+            extraConfig = ''
+              proxy_set_header X-Forwarded-Proto $scheme;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            '';
+            
+          };
         }; 
         "list.desgrandsvoisins.org" = {
           serverAliases = ["list.desgrandsvoisins.com"];
