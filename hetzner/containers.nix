a570d5c162608638daf4bf1243d9ea2859d68d10
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-24 07:56:23 +0100

    Test container

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -229,6 +229,34 @@ in
     
   # };
 
+  containers.test = {
+    autoStart = true;
+    privateNetwork = true;
+    config = { config, pkgs, ... }: {
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+        nginx
+        ];
+    };
+  };
+
   containers.wagtail = {
     
     
