06612047f6a4ba5a0a0477af034eea77aee5f86d
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-24 21:34:51 +0100

    username

--- hetzner/httpd.nix
+++ hetzner/httpd.nix
@@ -67,7 +67,7 @@ in
         AuthType openid-connect
         Require valid-user
         ProxyPass "http://localhost:8334/"
-        RequestHeader set X-FileBrowser-User %{env:OIDC_CLAIM_sub}  
+        RequestHeader set X-FileBrowser-User %{env:OIDC_CLAIM_username}  
       </Location>
     '';
   };
