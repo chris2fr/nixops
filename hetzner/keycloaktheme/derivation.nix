02fd11d0f5ec9438febbc79cb532f55324778301
Chris Mann <chris@lesgrandsvoisins.com>
2023-12-30 17:42:09 +0100

    Working on the theme

--- hetzner/keycloaktheme/derivation.nix
+++ hetzner/keycloaktheme/derivation.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-  for i in account admin email login welcome
+  for i in account admin email login welcome static static/css static/js static/images static/fonts static/fonts/fengardoneue static/fonts/lack
   do
   echo $i
   mkdir -p $out/$i
