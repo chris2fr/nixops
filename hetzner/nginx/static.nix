424a7c2d198e845a2c570cfc8e84d17bbe4e8d58
Chris <christopher@mann.fr>
2023-12-31 21:28:00 +0100

    Update static.nix

--- hetzner/nginx/static.nix
+++ hetzner/nginx/static.nix
@@ -5,7 +5,7 @@ in
   services.nginx.virtualHosts = {
     "doc.desgrandsvoisins.org" = {
       serverAliases = ["resdigita.com" "resdigita.org" "www.resdigita.org" "doc.desgrandsvoisins.com"  "doc.lesgrandsvoisins.com" "doc.resdigita.com"];
-       globalRedirect = "doc.resdigita.com";
+       globalRedirect = "quartz.resdigita.com";
       enableACME = true;
       forceSSL = true;
       root = "/var/www/resdigitacom";
