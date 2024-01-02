be9d68bc9d1b7ac276b02e72b18db987a420bdf9
Chris Mann <chris@lesgrandsvoisins.com>
2024-01-02 13:31:00 +0100

    /var/log/haproxy

--- hetzner/configuration.nix
+++ hetzner/configuration.nix
@@ -292,13 +292,13 @@ in
           timeout connect 10s
           timeout client  60s
           timeout server  60s
-          errorfile 400 /etc/haproxy/errors/400.http
-          errorfile 403 /etc/haproxy/errors/403.http
-          errorfile 408 /etc/haproxy/errors/408.http
-          errorfile 500 /etc/haproxy/errors/500.http
-          errorfile 502 /etc/haproxy/errors/502.http
-          errorfile 503 /etc/haproxy/errors/503.http
-          errorfile 504 /etc/haproxy/errors/504.http
+          errorfile 400 /var/log/haproxy/errors/400.http
+          errorfile 403 /var/log/haproxy/errors/403.http
+          errorfile 408 /var/log/haproxy/errors/408.http
+          errorfile 500 /var/log/haproxy/errors/500.http
+          errorfile 502 /var/log/haproxy/errors/502.http
+          errorfile 503 /var/log/haproxy/errors/503.http
+          errorfile 504 /var/log/haproxy/errors/504.http
 
         frontend http-in
           bind *:9080
