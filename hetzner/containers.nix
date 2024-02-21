2e7d0ee72b69d7d0cb493f091090c78b8bc1ed51
Chris Mann <chris@lesgrandsvoisins.com>
2024-02-21 22:30:31 +0100

    Kopia system backup

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -143,21 +143,33 @@ in
         #   };
         # };
       };
-      systemd.services.silverbullet = {
-        description = "SilverBullet.Resdigita.com";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          WorkingDirectory = "/home/silverbullet/.nix-profile/bin/";
-          Environment = "PATH=/home/silverbullet/.deno/bin:/run/wrappers/bin:/home/silverbullet/.nix-profile/bin:/nix/profile/bin:/home/silverbullet/.local/state/nix/profile/bin:/etc/profiles/per-user/silverbullet/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin;";
-          ExecStart = ''/home/silverbullet/.deno/bin/silverbullet -L 192.168.102.2 /home/silverbullet/quartz/'';
-          Restart = "always";
-          RestartSec = "10s";
-          User = "silverbullet";
-          Group = "users";
+      systemd = {
+        user.services.kopia = {
+          description = "Kopia Snapshot of Silverbullet";
+          script = ''
+            /run/current-system/sw/bin/kopia repository connect from-config --token `cat /home/silverbullet/.secret.kopia`
+            /run/current-system/sw/bin/kopia snapshot create /home/silverbullet/quartz/
+          '';
+          wants = "network-online.target";
+          after = "network-online.target";
+          
         };
-        unitConfig = {
-          StartLimitInterval = "1min";
+        services.silverbullet = {
+          description = "SilverBullet.Resdigita.com";
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            WorkingDirectory = "/home/silverbullet/.nix-profile/bin/";
+            Environment = "PATH=/home/silverbullet/.deno/bin:/run/wrappers/bin:/home/silverbullet/.nix-profile/bin:/nix/profile/bin:/home/silverbullet/.local/state/nix/profile/bin:/etc/profiles/per-user/silverbullet/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin;";
+            ExecStart = ''/home/silverbullet/.deno/bin/silverbullet -L 192.168.102.2 /home/silverbullet/quartz/'';
+            Restart = "always";
+            RestartSec = "10s";
+            User = "silverbullet";
+            Group = "users";
+          };
+          unitConfig = {
+            StartLimitInterval = "1min";
+          };
         };
       };
     };
