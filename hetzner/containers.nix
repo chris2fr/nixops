b8dd31a865e0584ba3b8a6e9aef49957ea26cde1
Chris Mann <chris@lesgrandsvoisins.com>
2024-02-21 10:34:26 +0100

    New Virtual Machine for SilverBullet

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -41,6 +41,176 @@ in
     # Lazy IPv6 connectivity for the container
     enableIPv6 = true;
   };
+  networking.interfaces.br2 = {
+    virtual = true;
+    ipv4.addresses = [
+	    { address="192.168.102.1"; prefixLength=24; } 
+    ];
+    ipv6.addresses = [
+      { address="fc00::2:1"; prefixLength=112; } 
+    ];
+  };
+  containers.silverbullet = {
+    autoStart = true;
+    privateNetwork = true;
+    hostBridge = "br2";
+    hostAddress = "192.168.102.1";
+    localAddress = "192.168.102.2";
+    hostAddress6 = "fc00::2:1";
+    localAddress6 = "fc00::2:2";
+    config = { config, pkgs, ... }: {
+      networking.firewall.allowedTCPPorts = [ 22 25 80 443 143 587 993 995 636 8443 9443 ];
+      users.users.wagtail.uid = 1003;
+      # users.groups.users.gid = 1003;
+      nix.settings.experimental-features = "nix-command flakes";
+      time.timeZone = "Europe/Amsterdam";
+      system.stateVersion = "23.11";
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+        python311
+        busybox
+        curl
+        wget
+        lynx
+        dig    
+        git
+        tmux
+        killall
+        pwgen
+        gettext
+        ];
+      users.users.mannchri.isNormalUser = true;
+    };
+  };
+  containers.wagtail = {
+    autoStart = true;
+    # privateNetwork = true;
+    # hostBridge = "br0";
+    # hostAddress = "192.168.100.10";
+    # localAddress = "192.168.100.11";
+    # hostAddress6 = "fc00::1";
+    # localAddress6 = "fc00::2";
+    bindMounts = { 
+      "/var/www/wagtail" = { 
+        hostPath = "/var/www/wagtail";
+        isReadOnly = false; 
+       }; 
+     };
+    config = { config, pkgs, ... }: {
+      networking.firewall.allowedTCPPorts = [ 22 25 80 443 143 587 993 995 636 8443 9443 ];
+      users.users.wagtail.uid = 1003;
+      # users.groups.users.gid = 1003;
+      nix.settings.experimental-features = "nix-command flakes";
+      time.timeZone = "Europe/Amsterdam";
+      system.stateVersion = "23.11";
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+        python311
+        python311Packages.pillow
+        python311Packages.gunicorn
+        python311Packages.pip
+        libjpeg
+        zlib
+        libtiff
+        freetype
+        python311Packages.venvShellHook
+        curl
+        wget
+        lynx
+        dig    
+        python311Packages.pylibjpeg-libjpeg
+        git
+        tmux
+        bat
+        cowsay
+        lzlib
+        killall
+        pwgen
+        python311Packages.pypdf2
+        python311Packages.python-ldap
+        python311Packages.pq
+        python311Packages.aiosasl
+        python311Packages.psycopg2
+        gettext
+        sqlite
+        postgresql_14
+        ];
+
+      # networking = {
+      #   firewall = {
+      #     enable = false;
+      #     allowedTCPPorts = [ 80 443 ];
+      #   };
+        # Use systemd-resolved inside the container
+        # useHostResolvConf = lib.mkForce false;
+      #};
+        
+      # services.resolved.enable = true;
+
+      # services.postgresql = {
+      #   enable = true;
+      #   enableTCPIP = true;
+      #   ensureDatabases = [
+      #     "wagtail"
+      #     "previous"
+      #     "fairemain"
+      #   ];
+          # # ensureDBOwnership = true;
+      # };
+      users.users.wagtail.isNormalUser = true;
+      systemd.services.wagtail = {
+        description = "Les Grands Voisins Wagtail Website";
+        after = [ "network.target" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          WorkingDirectory = "/home/wagtail/wagtail-lesgv/";
+          # ExecStart = ''/home/wagtail/venv/bin/gunicorn --env WAGTAIL_ENV='production' --access-logfile access.log --chdir /home/wagtail/wagtail-lesgv --workers 3 --bind unix:/var/lib/wagtail/wagtail-lesgv.sock lesgv.wsgi:application'';
+          ExecStart = ''/home/wagtail/venv/bin/gunicorn --env WAGTAIL_ENV='production' --access-logfile /var/log/wagtail/access.log --error-logfile /var/log/wagtail/error.log --chdir /home/wagtail/wagtail-lesgv --workers 12 --bind 127.0.0.1:8000 lesgv.wsgi:application'';
+          Restart = "always";
+          RestartSec = "10s";
+          User = "wagtail";
+          Group = "users";
+        };
+        unitConfig = {
+          StartLimitInterval = "1min";
+        };
+      };
+    };
+  };
   containers.seafile = {
     autoStart = true;
     privateNetwork = true;
@@ -185,6 +355,8 @@ in
     };
   };    
 
+
+
   # containers.filestash = {
   #   autoStart = true;
   #   privateNetwork = true;
@@ -245,118 +417,6 @@ in
   #     };
   #   };
   # };
-  containers.wagtail = {
-    autoStart = true;
-    # privateNetwork = true;
-    # hostBridge = "br0";
-    # hostAddress = "192.168.100.10";
-    # localAddress = "192.168.100.11";
-    # hostAddress6 = "fc00::1";
-    # localAddress6 = "fc00::2";
-    bindMounts = { 
-      "/var/www/wagtail" = { 
-        hostPath = "/var/www/wagtail";
-        isReadOnly = false; 
-       }; 
-     };
-    config = { config, pkgs, ... }: {
-      networking.firewall.allowedTCPPorts = [ 22 25 80 443 143 587 993 995 636 8443 9443 ];
-      users.users.wagtail.uid = 1003;
-      # users.groups.users.gid = 1003;
-      nix.settings.experimental-features = "nix-command flakes";
-      time.timeZone = "Europe/Amsterdam";
-      system.stateVersion = "23.11";
-      environment.systemPackages = with pkgs; [
-        ((vim_configurable.override {  }).customize{
-          name = "vim";
-          vimrcConfig.customRC = ''
-            " your custom vimrc
-            set mouse=a
-            set nocompatible
-            colo torte
-            syntax on
-            set tabstop     =2
-            set softtabstop =2
-            set shiftwidth  =2
-            set expandtab
-            set autoindent
-            set smartindent
-            " ...
-          '';
-          }
-        )
-        python311
-        python311Packages.pillow
-        python311Packages.gunicorn
-        python311Packages.pip
-        libjpeg
-        zlib
-        libtiff
-        freetype
-        python311Packages.venvShellHook
-        curl
-        wget
-        lynx
-        dig    
-        python311Packages.pylibjpeg-libjpeg
-        git
-        tmux
-        bat
-        cowsay
-        lzlib
-        killall
-        pwgen
-        python311Packages.pypdf2
-        python311Packages.python-ldap
-        python311Packages.pq
-        python311Packages.aiosasl
-        python311Packages.psycopg2
-        gettext
-        sqlite
-        postgresql_14
-        ];
-
-      # networking = {
-      #   firewall = {
-      #     enable = false;
-      #     allowedTCPPorts = [ 80 443 ];
-      #   };
-        # Use systemd-resolved inside the container
-        # useHostResolvConf = lib.mkForce false;
-      #};
-        
-      # services.resolved.enable = true;
-
-      # services.postgresql = {
-      #   enable = true;
-      #   enableTCPIP = true;
-      #   ensureDatabases = [
-      #     "wagtail"
-      #     "previous"
-      #     "fairemain"
-      #   ];
-          # # ensureDBOwnership = true;
-      # };
-      users.users.wagtail.isNormalUser = true;
-      systemd.services.wagtail = {
-        description = "Les Grands Voisins Wagtail Website";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          WorkingDirectory = "/home/wagtail/wagtail-lesgv/";
-          # ExecStart = ''/home/wagtail/venv/bin/gunicorn --env WAGTAIL_ENV='production' --access-logfile access.log --chdir /home/wagtail/wagtail-lesgv --workers 3 --bind unix:/var/lib/wagtail/wagtail-lesgv.sock lesgv.wsgi:application'';
-          ExecStart = ''/home/wagtail/venv/bin/gunicorn --env WAGTAIL_ENV='production' --access-logfile /var/log/wagtail/access.log --error-logfile /var/log/wagtail/error.log --chdir /home/wagtail/wagtail-lesgv --workers 12 --bind 127.0.0.1:8000 lesgv.wsgi:application'';
-          Restart = "always";
-          RestartSec = "10s";
-          User = "wagtail";
-          Group = "users";
-        };
-        unitConfig = {
-          StartLimitInterval = "1min";
-        };
-      };
-    };
-  };
 }
 
   # networking.nat = {
