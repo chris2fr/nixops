08e80e42d90e6b129126e57aa1d78a96a0eba7fb
Chris Mann <chris@lesgrandsvoisins.com>
2024-02-21 13:06:36 +0100

    Path quickfix

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -132,7 +132,7 @@ in
         serviceConfig = {
           WorkingDirectory = "/home/silverbullet/";
           # ExecStart = ''/home/wagtail/venv/bin/gunicorn --env WAGTAIL_ENV='production' --access-logfile access.log --chdir /home/wagtail/wagtail-lesgv --workers 3 --bind unix:/var/lib/wagtail/wagtail-lesgv.sock lesgv.wsgi:application'';
-          ExecStart = ''/home/silverbullet/.deno/bin/silverbullet /home/silverbullet/quartz/resdigita'';
+          ExecStart = ''PATH=/home/silverbullet/.deno/bin:/run/wrappers/bin:/home/silverbullet/.nix-profile/bin:/nix/profile/bin:/home/silverbullet/.local/state/nix/profile/bin:/etc/profiles/per-user/silverbullet/bin:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin /home/silverbullet/.deno/bin/silverbullet /home/silverbullet/quartz/resdigita'';
           Restart = "always";
           RestartSec = "10s";
           User = "silverbullet";
