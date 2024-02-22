59c61314da0c23f4c6a03ff8e09208665bfac572
Chris Mann <chris@lesgrandsvoisins.com>
2024-02-22 11:29:25 +0100

    proxy_redirect

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -82,7 +82,7 @@ in
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header   X-Real-IP $remote_addr;
               proxy_set_header   Host $host;
-              # proxy_redirect off;
+              proxy_redirect off;
             '';
           };
         };
