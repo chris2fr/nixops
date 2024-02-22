d8e20b8905db213d4ae5cb5c63d9cda87a52ff9c
Chris Mann <chris@lesgrandsvoisins.com>
2024-02-22 09:07:47 +0100

    Migrating Wordpress

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -33,6 +33,9 @@ let
     simplejson
     python3-gnutls
   ];
+  mannchriRsaPublic = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuBWybYSoR6wyd1EG5YnHPaMKE3RQufrK7ycej7avw3Ug8w8Ppx2BgRGNR6EamJUPnHEHfN7ZZCKbrAnuP3ar8mKD7wqB2MxVqhSWvElkwwurlijgKiegYcdDXP0JjypzC7M73Cus3sZT+LgiUp97d6p3fYYOIG7cx19TEKfNzr1zHPeTYPAt5a1Kkb663gCWEfSNuRjD2OKwueeNebbNN/OzFSZMzjT7wBbxLb33QnpW05nXlLhwpfmZ/CVDNCsjVD1+NXWWmQtpRCzETL6uOgirhbXYW8UyihsnvNX8acMSYTT9AA3jpJRrUEMum2VizCkKh7bz87x7gsdA4wF0/w== rsa-key-20220407";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
+  hasaeraRsaPublic = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuBWybYSoR6wyd1EG5YnHPaMKE3RQufrK7ycej7avw3Ug8w8Ppx2BgRGNR6EamJUPnHEHfN7ZZCKbrAnuP3ar8mKD7wqB2MxVqhSWvElkwwurlijgKiegYcdDXP0JjypzC7M73Cus3sZT+LgiUp97d6p3fYYOIG7cx19TEKfNzr1zHPeTYPAt5a1Kkb663gCWEfSNuRjD2OKwueeNebbNN/OzFSZMzjT7wBbxLb33QnpW05nXlLhwpfmZ/CVDNCsjVD1+NXWWmQtpRCzETL6uOgirhbXYW8UyihsnvNX8acMSYTT9AA3jpJRrUEMum2VizCkKh7bz87x7gsdA4wF0/w== rsa-key-20220407";
 in
 {
   networking.nat = {
@@ -51,6 +54,191 @@ in
   #     { address="fc00::2:1"; prefixLength=112; } 
   #   ];
   # };
+  containers.wordpress = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.103.1";
+    localAddress = "192.168.103.2";
+    hostAddress6 = "fc00::3:1";
+    localAddress6 = "fc00::3:2"; 
+    config = { config, pkgs, lib, ... }: {
+      imports = [ (import "${home-manager}/nixos") ];
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+        cowsay
+        home-manager
+        curl
+        wget
+        lynx
+        git
+        tmux
+        bat
+        python311Packages.pillow
+        python311Packages.pylibjpeg-libjpeg
+        zlib
+        lzlib
+        dig
+        killall
+        pwgen
+        openldap
+        mysql80
+        python311Packages.pypdf2
+        python311Packages.python-ldap
+        python311Packages.pq
+        python311Packages.aiosasl
+        python311Packages.psycopg2
+        mariadb
+        (pkgs.php82.buildEnv {
+          extensions = ({ enabled, all }: enabled ++ (with all; [
+            imagick
+          ]));
+          extraConfig = ''
+          '';
+        })
+        php82Extensions.imagick
+      ];
+      networking = {
+        hostName = "wordpress.resdigita.com"; 
+        firewall.allowedTCPPorts = [ 22 25 80 443 143 587 993 995 636 ];
+      };
+      system = {
+        copySystemConfiguration = true;
+        stateVersion = "23.05";
+      };
+      environment.sessionVariables = rec {
+        EDITOR="vim";
+        WAGTAIL_ENV = "production";
+      };
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = "contact@lesgrandsvoisins.com";
+        defaults.webroot = "/var/www";
+      };
+      ## Adding Linux Containers
+      virtualisation = {
+        lxd.enable = true;
+        lxc.enable = true;
+        lxc.lxcfs.enable = true;
+      };
+      time.timeZone = "Europe/Paris";
+      i18n.defaultLocale = "fr_FR.UTF-8";
+      users.users.mannchri = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [ mannchriRsaPublic ];
+        extraGroups = [ "wheel" ];
+      };
+      users.users.hasaera = {
+        isNormalUser = true;
+        openssh.authorizedKeys.keys = [ hasaeraRsaPublic ];
+        extraGroups = [ "wheel" ];
+      };
+      home-manager.users.mannchri = {pkgs, ...}: {
+        home.packages = [ pkgs.atool pkgs.httpie ];
+        home.stateVersion = "23.05";
+        programs.home-manager.enable = true;
+        programs.vim = {
+          enable = true;
+          plugins = with pkgs.vimPlugins; [ vim-airline ];
+          settings = { ignorecase = true; tabstop = 2; };
+          extraConfig = ''
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+          '';
+        };
+      };
+      home-manager.users.hasaera = {pkgs, ...}: {
+        home.packages = [ pkgs.atool pkgs.httpie ];
+        home.stateVersion = "23.05";
+        programs.home-manager.enable = true;
+        programs.vim = {
+          enable = true;
+          plugins = with pkgs.vimPlugins; [ vim-airline ];
+          settings = { ignorecase = true; tabstop = 2; };
+          extraConfig = ''
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+          '';
+        };
+      };
+
+      services = {
+        openssh = {
+          enable = true;
+          settins.PermitRootLogin = "prohibit-password";
+        };
+        httpd = {
+          enable = true; 
+          enablePHP = true;
+          phpPackage = pkgs.php.buildEnv {
+            extensions = ({ enabled, all }: enabled ++ (with all; [
+                imagick
+            ]));
+            extraConfig = ''
+            '';
+          };
+        };
+      };
+    };
+  };
+
+  services.httpd.phpOptions = ''
+    upload_max_filesize = 128M
+    post_max_size = 256M
+    max_execution_time = 300
+  '';
+  services.httpd.virtualHosts."vpsfreecz003.lesgrandsvoisins.com" = {
+    serverAliases = [
+      "ghh.villagevoisin.com"
+    ];
+    enableACME = true;
+    forceSSL = true;
+    documentRoot = "/var/www/ghh";
+    extraConfig = ''
+      <Directory /var/www/ghh>
+        DirectoryIndex index.php
+        Require all granted
+        AllowOverride FileInfo
+        FallbackResource /index.php
+      </Directory>
+      '';
+  };
+  services.mysql.package = pkgs.mariadb;
+  services.mysql.enable = true;
+    };
+  };
   containers.silverbullet = {
     autoStart = true;
     privateNetwork = true;
