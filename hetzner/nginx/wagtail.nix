b44278832248180328291c48467ea1a911f41b99
Chris Mann <chris@lesgrandsvoisins.com>
2024-03-04 16:17:06 +0100

    Condition

--- hetzner/nginx/wagtail.nix
+++ hetzner/nginx/wagtail.nix
@@ -204,7 +204,7 @@ in
       if () {
           return 301 $scheme://www.$host$request_uri;
       }
-      if ($host = 'lesgv.org' | $host = 'lesgv.com' | $host = 'www.lesgv.com' | $host = 'www.lesgv.org' | $host = 'gv.coop' | $host = 'www.gv.coop'  | $host = 'coopgv.com'  | $host = 'coopgv.org'  | $host = 'www.coopgv.com'  | $host = 'www.coopgv.org' ) {
+      if (($host = 'lesgv.org')|($host = 'lesgv.com')|($host = 'www.lesgv.com')|($host = 'www.lesgv.org')|($host = 'gv.coop')|($host = 'www.gv.coop' )|($host = 'coopgv.com' )|($host = 'coopgv.org' )|($host = 'www.coopgv.com' )|($host = 'www.coopgv.org' )) {
           # return 301 $scheme://les.$host$request_uri;
           return 301 $scheme://les.gv.coop$request_uri;
       }
