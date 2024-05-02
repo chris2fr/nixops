e5fa8f0b9cf4741d47e78eaf96aa1be3fe74168f
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-02 09:35:06 +0200

    syncthing

--- hetzner/configuration.nix
+++ hetzner/configuration.nix
@@ -289,6 +289,10 @@ in
   services= {
     syncthing = {
       enable = true;
+      guiAddress = "syncthing.resdigita.com";
+      group = "wwwrun";
+      cert = "/var/lib/acme/syncthing.resdigita.com/fullchain.pem";
+      key = "/var/lib/acme/syncthing.resdigita.com/key.pem";
 #      folders = {
 #        "/var/local/syncthing" = {
 #          id = "syncthing";
