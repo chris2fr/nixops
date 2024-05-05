ced733e02e062589c1f5e1ba8a849e56acab1dbe
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-05 09:27:06 +0200

    Nix stuff

--- hetzner/configuration.nix
+++ hetzner/configuration.nix
@@ -76,14 +76,14 @@ in
     };
     # firewall.enable = false;
     firewall.trustedInterfaces = [ "docker0" "lxdbr1" "lxdbr0" "ve-silverbullet"];
-    firewall.allowedTCPPorts = [ 22 25 80 443 143 587 993 995]
-    + [ 636 8443 9080 9443 10080 10443 11443 ]
     # Syncthing ports: 8384 for remote access to GUI
     # 22000 TCP and/or UDP for sync traffic
     # 21027/UDP for discovery
     # source: https://docs.syncthing.net/users/firewall.html
-    + [ 8384 22000 ]
-    + [ 22000 21027 ];
+    firewall.allowedTCPPorts = [ 22 25 80 443 143 587 993 995
+    636 8443 9080 9443 10080 10443 11443
+    8384 22000 
+    22000 21027 ];
     # interfaces."eno1".ipv6 = {
 
     # }
