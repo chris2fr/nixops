45894bab33bf629689d71af9a260a981ba702db5
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-28 22:06:08 +0200

    Proxy issues

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -5,7 +5,7 @@ nginxLocationWagtailExtraConfig = ''
     proxy_set_header X-Forwarded-Proto $scheme;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
     # proxy_http_version 1.1;
-    # proxy_set_header Host $host;
+    proxy_set_header Host $host;
     # proxy_set_header Upgrade $http_upgrade;
     # proxy_set_header Connection $connection_upgrade_keepalive;
 '';
--- hetzner/nginx/wagtail.nix
+++ hetzner/nginx/wagtail.nix
@@ -1,12 +1,12 @@
 { config, pkgs, lib, ... }:
 let 
 nginxLocationWagtailExtraConfig = ''
-    # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_redirect off;
-    # proxy_http_version 1.1;
-    proxy_set_header X-Forwarded-Proto $scheme;
     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header Host $host;
+    proxy_redirect off;
+    proxy_http_version 1.1;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    # proxy_set_header Host $host;
     # proxy_set_header Upgrade $http_upgrade;
     # proxy_set_header Connection $connection_upgrade_keepalive;
 '';
