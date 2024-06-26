4607009aeeb57fc6efbe420a7754b011446e0c2c
Chris Mann <chris@lesgtrandsvoisins.com>
2024-06-26 18:09:02 +0200

    discourse.village.ngo

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -334,13 +334,21 @@ in
           enableACME = true;
           forceSSL = true;
           locations."/" = {
-            proxyPass = "http://192.168.104.11/";
             extraConfig = ''
-              proxy_set_header Host $host;
               proxy_http_version 1.1;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
               proxy_set_header X-Forwarded-Proto $scheme;
               proxy_set_header X-Real-IP $remote_addr;
+              proxy_redirect off;
+              proxy_set_header   X-Real-IP $remote_addr;
+              proxy_set_header   Host $host;
+              proxy_pass         http://192.168.104.11:80/;
+              proxy_http_version 1.1;
+              proxy_set_header   Upgrade $http_upgrade;
+              proxy_set_header   Connection "upgrade";
+              # proxy_set_header X-Forwarded-Proto $scheme;
+              # proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              # proxy_redirect off;
           '';
           };
         };
