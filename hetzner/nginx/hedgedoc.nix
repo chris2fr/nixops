0ff9a4129b68dd95b8edcce95bd29c6d5493be4f
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-16 12:39:46 +0200

    hedgedoc.gv.coop

--- hetzner/nginx/hedgedoc.nix
+++ hetzner/nginx/hedgedoc.nix
@@ -20,21 +20,21 @@ in
       #   ];
       #   globalRedirect = "hedgedoc.resdigita.com";
       # };
-      "hedgedoc.village.ngo" = {
-        serverAliases = ["hedgedoc.lesgv.org" "hedgedoc.resdigita.com"];
+      "hedgedoc.gv.coop" = {
+        serverAliases = ["hedgedoc.lesgv.org" "hedgedoc.resdigita.com" "hedgedoc.village.ngo"];
         enableACME = true;
         forceSSL = true;
         locations."/".proxyPass = "http://localhost:3333/";
         extraConfig = ''
-          if ($host != "hedgedoc.village.ngo") {
-            return 302 $scheme://hedgedoc.village.ngo$request_uri;
+          if ($host != "hedgedoc.gv.coop") {
+            return 302 $scheme://hedgedoc.gv.coop$request_uri;
           }
         '';
       };
-       "hedgedoc.gv.coop" = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/".proxyPass = "http://localhost:3333/";
-      };
+      #  "hedgedoc.gv.coop" = {
+      #   enableACME = true;
+      #   forceSSL = true;
+      #   locations."/".proxyPass = "http://localhost:3333/";
+      # };
   };
 }
