7225335f8ca7a93a244446932e0a6efb9a4b4e8e
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-17 16:21:32 +0200

    Not sure about why the proxy user updates

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -1420,7 +1420,7 @@ in
                       by * read''
                   ''{3}to attrs=userPassword
                       by self write
-                      by dn.exact="cn=admin@lesgrandsvoisins.com,ou=users,${ldapBaseDN}" manage
+                      by dn.exact="cn=admin@lesgrandsvoisins.com,ou=users,${ldapBaseDN}" write
                       by anonymous auth
                       by * none''
                   ''{4}to *
