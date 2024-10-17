ed4ca07ab6350239b811148aca1e9882df2d590c
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-17 19:27:06 +0200

    Mailserver

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -38,6 +38,22 @@ let
   hasaeraRsaPublic = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuBWybYSoR6wyd1EG5YnHPaMKE3RQufrK7ycej7avw3Ug8w8Ppx2BgRGNR6EamJUPnHEHfN7ZZCKbrAnuP3ar8mKD7wqB2MxVqhSWvElkwwurlijgKiegYcdDXP0JjypzC7M73Cus3sZT+LgiUp97d6p3fYYOIG7cx19TEKfNzr1zHPeTYPAt5a1Kkb663gCWEfSNuRjD2OKwueeNebbNN/OzFSZMzjT7wBbxLb33QnpW05nXlLhwpfmZ/CVDNCsjVD1+NXWWmQtpRCzETL6uOgirhbXYW8UyihsnvNX8acMSYTT9AA3jpJRrUEMum2VizCkKh7bz87x7gsdA4wF0/w== rsa-key-20220407";
   ldapDomainName = "ldap.gv.coop";
   ldapBaseDN = "dc=gv,dc=coop";
+  bindPassword = (lib.removeSuffix "\n" (builtins.readFile /etc/nixos/.secrets.bind));
+  alicePassword = (lib.removeSuffix "\n" (builtins.readFile /etc/nixos/.secrets.alice));
+  bobPassword = (lib.removeSuffix "\n" (builtins.readFile /etc/nixos/.secrets.bob));
+  sogoPassword = (lib.removeSuffix "\n" (builtins.readFile /etc/nixos/.secrets.sogo));
+  oauthPassword = (lib.removeSuffix "\n" (builtins.readFile /etc/nixos/.secrets.oauthpassword));
+  domainName = "mail.gv.coop";
+  whitelistSubnets =  [ 
+      "10.0.0.0/8" 
+      "172.16.0.0/12" 
+      "192.168.0.0/16"
+      "8.8.8.8" # Resolves the IP via DNS
+      "mail.gv.coop" 
+  ];
+  mailServerDomainAliases = [ 
+    "gv.coop"
+  ];
 in
 {
   networking = {
@@ -1230,15 +1246,16 @@ in
     config = { config, pkgs, lib, ...  }: {
       nix.settings.experimental-features = "nix-command flakes";
       system.stateVersion = "24.05";
-      networking = {
-        firewall.allowedTCPPorts = [ 18080 10389 10686 22 ];
-        # hosts = {
-        #   "192.168.107.11" = ["ldap.gv.coop"];
-        # };
-        # useHostResolvConf = true;
-        # useHostResolvConf = lib.mkForce false;
-        # resolvconf.enable = true;
-      };
+      # networking = {
+      #   firewall.allowedTCPPorts = [ 18080 10389 10686 22 80 443 389 636 993 11211 25 ];
+      #   trustedInterfaces = ["eno1" "lo"];
+      #   # hosts = {
+      #   #   "192.168.107.11" = ["ldap.gv.coop"];
+      #   # };
+      #   # useHostResolvConf = true;
+      #   # useHostResolvConf = lib.mkForce false;
+      #   # resolvconf.enable = true;
+      # };
       time.timeZone = "Europe/Paris";
       environment.systemPackages = with pkgs; [
         lynx
@@ -1263,6 +1280,14 @@ in
           '';
           }
         )
+        # postgresql_14
+        pwgen
+      ];
+      imports = [
+        (builtins.fetchTarball {
+          url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/ldap-support/nixos-mailserver-nixos-24.05.tar.gz";
+          sha256 = "sha256:15v6b5z8gjspps5hyq16bffbwmq0rwfwmdhyz23frfcni3qkgzpc";
+        })
       ];
       users = {
         groups = {
@@ -1278,13 +1303,157 @@ in
             uid = 54;
             group = "wwwrun";
           };
+          dovecot2.extraGroups = ["wwwrun"];
         };
       };
       systemd.tmpfiles.rules = [
         "d /var/lib/acme/${ldapDomainName} 0755 acme wwwrun"
         "f /var/lib/openldap/pmw/schema/pwm.ldif 0755 openldap openldap"
       ];
+      mailserver = {
+        enable = true;
+        fqdn = domainName;
+        domains = mailServerDomainAliases;
+        certificateScheme = "acme";
+        certificateFile = "/var/lib/acme/${domainName}/fullchain.pem";
+        certificateDirectory = "/var/lib/acme/${domainName}/";
+        keyFile =  "/var/lib/acme/${domainName}/key.pem"; 
+        messageSizeLimit = 209715200;
+        indexDir = "/var/lib/dovecot/indices";
+        ldap = {
+          enable = true;
+          bind = {
+            dn = "cn=admin,${ldapBaseDCDN}";
+            passwordFile = "/etc/nixos/.secrets.bind";
+          };
+          uris = [
+            "ldap:///"
+          ];
+          searchBase = "ou=users,${ldapBaseDCDN}";
+          searchScope = "sub";
+          tlsCAFile = "/var/lib/acme/${domainName}/fullchain.pem";
+          startTls = false;
+          postfix = {
+            mailAttribute = "mail";
+            uidAttribute = "uid";
+            #  filter = "(cn=%s)";
+          };
+        };
+        fullTextSearch = {
+          enable = true;
+          # index new email as they arrive
+          autoIndex = true;
+          # this only applies to plain text attachments, binary attachments are never indexed
+          indexAttachments = false;
+          enforced = "yes";
+          memoryLimit = 2000;
+        };
+        services.postfix.config.maillog_file = "/var/log/postfix.log";
+        services.postfix.masterConfig.postlog = {
+          command = "postlogd";
+          type = "unix-dgram";
+          privileged = true;
+          private = false;
+          chroot = false;
+          maxproc = 1;
+        };
+      };
+      systemd.extraConfig = ''
+        DefaultTimeoutStartSec=600s
+      '';
       services = {
+        fail2ban = {
+          enable = true;
+          maxretry = 5; # Observe 5 violations before banning an IP
+          ignoreIP = whitelistSubnets;
+          bantime = "24h"; # Set bantime to one day
+          bantime-increment = {
+            enable = true; # Enable increment of bantime after each violation
+            formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+            # multipliers = "1 2 4 8 16 32 64";
+            maxtime = "168h"; # Do not ban for more than 1 week
+            overalljails = true; # Calculate the bantime based on all the violations
+          };
+          jails = {
+            apache-nohome-iptables = ''
+              # Block an IP address if it accesses a non-existent
+              # home directory more than 5 times in 10 minutes,
+              # since that indicates that it's scanning.
+              filter = apache-nohome
+              action = iptables-multiport[name=HTTP, port="http,https"]
+              logpath = /var/log/httpd/error_log*
+              backend = auto
+              findtime = 600
+              bantime  = 600
+              maxretry = 5
+            '';
+            postfix = ''
+              port     = smtp,465,submission,imap,imaps,pop3,pop3s
+              action = iptables-multiport[name=HTTP, port="smtp,465,submission,imap,imaps,pop3,pop3s"]
+              logpath  = /var/log/postfix.log
+              backend  = auto
+              enabled  = true
+              filter   = postfix[mode=auth]
+              mode     = more
+            '';
+            # dovecot = ''
+            #   port     = smtp,465,submission
+            #   logpath  = /var/log/fail2ban.log
+            #   backend  = auto
+            #   enabled  = true
+            #   mode     = more
+            # '';
+            # postfix-sasl = ''
+            #   filter   = postfix[mode=auth]
+            #   port     = smtp,465,submission,imap,imaps,pop3,pop3s
+            #   logpath  = /var/log/fail2ban.log
+            #   backend  = auto
+            #   enabled  = true
+            #   mode     = more
+            # '';
+          };
+        };
+        roundcube = {
+          enable = true;
+          # this is the url of the vhost, not necessarily the same as the fqdn of
+          # the mailserver
+          hostName = "mail.lesgrandsvoisins.com";
+          #  dicts =  [ en fr de ];
+          extraConfig = ''
+              # starttls needed for authentication, so the fqdn required to match
+              # the certificate
+              $config['smtp_server'] = "tls://mail.lesgrandsvoisins.com";
+              $config['smtp_user'] = "%u";
+              $config['smtp_pass'] = "%p";
+              # $config['oauth_provider'] = 'generic';
+              # $config['oauth_provider_name'] = 'authentik';
+              # $config['oauth_client_id'] = 'q3nTVQdV2ctY8GeNKvPuHokNa5RxT0VhZbVFCyY3';
+              # $config['oauth_client_secret'] = '${oauthPassword}';
+              # $config['oauth_auth_uri'] = 'https://authentik.resdigita.com/application/o/authorize/';
+              # $config['oauth_token_uri'] = 'https://authentik.resdigita.com/application/o/token/';
+              # $config['oauth_identity_uri'] = 'https://authentik.resdigita.com/application/o/userinfo/';
+              # $config['oauth_scope'] = "openid dovecotprofile email";
+              # $config['oauth_auth_parameters'] = [];
+              # $config['oauth_identity_fields'] = ['email'];
+              $config['generic_message_footer_html'] = '<a href="https://www.lesgrandsvoisins.com">Les Grands Voisins .com comme communautés</a>';
+              $config['session_samesite'] = "Lax";
+              $config['support_url'] = 'https://www.lesgrandsvoisins.com';
+              $config['product_name'] = 'Roundcube Webmail des GV';
+              $config['session_debug'] = true;
+              $config['session_domain'] = 'mail.lesgrandsvoisins.com';
+              $config['login_password_maxlen'] = 4096;
+          '';
+          dicts = [ pkgs.aspellDicts.fr pkgs.aspellDicts.en ];
+          maxAttachmentSize = 75;
+        };
+        memcached = {
+          enable = true;
+          # maxMemory = 256;
+          # enableUnixSocket = true;
+          # port = 11211;
+          # listen = "[::1]";
+          # user = "sogo";
+        };
         openssh = {
           enable = true;
         };
