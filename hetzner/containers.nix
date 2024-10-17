c20ddfa1e00a09367d29c6b4781e86d935c008a1
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-17 09:18:51 +0200

    OpenLDAP Container

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -959,7 +959,6 @@ in
       };
     };
   };
-
   containers.keycloak = {
     bindMounts = {
       "/var/lib/acme/keycloak.village.ngo/" = {
@@ -1053,6 +1052,203 @@ in
           # themes = {lesgv = (pkgs.callPackage "/etc/nixos/keycloaktheme/derivation.nix" {});};
         };
       };
+    };
+  };
+  systemd.tmpfiles.rules = [
+    "d /var/local/cherryldap 0755 cherryldap users"
+  ];
+  users.users.cherryldap = {
+    isNormalUser = true;
+    uid = 11111;
+  };
+  containers.cherryldap = {
+    autoStart = true;
+    privateNetwork = true;
+    hostAddress = "192.168.106.1";
+    localAddress = "192.168.106.2";
+    hostAddress6 = "fc00::6:1";
+    localAddress6 = "fc00::6:2"; 
+    bindMounts = { 
+      "/var/local/cherryldap" = { 
+        hostPath = "/var/local/cherryldap";
+        isReadOnly = false; 
+      }; 
+    };
+    config = { config, pkgs, ... }: {
+      nix.settings.experimental-features = "nix-command flakes";
+      time.timeZone = "Europe/Paris";
+      system.stateVersion = "24.05";
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+        # python311Packages.cherrypy-cors
+        # python311Packages.pillow
+        # python311Packages.gunicorn
+        # python311Packages.pip
+        # libjpeg
+        # zlib
+        # libtiff
+        # freetype
+        # python311Packages.venvShellHook
+        curl
+        wget
+        lynx
+        libclang
+        # dig    
+        # python311Packages.pylibjpeg-libjpeg
+        git
+        # tmux
+        # bat
+        # cowsay
+        # lzlib
+        # killall
+        # pwgen
+        # python311Packages.pypdf2
+        # python311Packages.pq
+        # python311Packages.aiosasl
+        # python311Packages.psycopg2
+        # gettext
+        # sqlite
+        # postgresql_14
+        # pipx
+        gnumake
+        gcc
+        glibcLocales
+        # python311Packages.manimpango
+        # python311Packages.devtools
+
+        # python311Packages.django-auth-ldap
+        # python311Packages.pq
+        # python311Packages.aiosasl
+        # python311Packages.pylibjpeg-libjpeg
+        # python311Packages.virtualenv
+        # python311Packages.toolz
+        # libpqxx
+        # postgresql
+        openldap
+        # python311Packages.pgcli
+        # cairo
+        # cairomm
+        # python311Packages.pycairo
+        # python311Packages.cairosvg
+        # python311Packages.cairocffi
+
+        gnutls
+        gnulib
+        gnumake
+        openssl
+        cyrus_sasl
+        # ldapcherry
+        (pkgs.python3.withPackages (python-pkgs: [
+          # python-pkgs.devtools
+          # python-pkgs.pydevtool
+          python-pkgs.cherrypy
+          python-pkgs.mako
+          python-pkgs.pyyaml
+          python-pkgs.python-ldap
+          python-pkgs.yq
+          python-pkgs.pip
+          # python-pkgs.python-ldap-test
+          # python-pkgs.ldappool
+          # python-pkgs.wheel
+          # python-pkgs.wheelUnpackHook
+          # python-pkgs.installer
+          # python-pkgs.bootstrap.installer
+          # python-pkgs.pandas
+          # python-pkgs.requests
+        ]))
+
+        ninja
+        cmake
+        # PHP
+        php
+        ];
+      networking = {
+        hostName = "cherryldap"; 
+        firewall.allowedTCPPorts = [ 22 25 53 80 443 143 587 993 995 636 ];
+        useHostResolvConf = lib.mkForce false;
+      };     
+      services.resolved.enable = true;   
+      users.users.cherryldap = {
+        isNormalUser = true;
+        uid = 11111;
+      };
+      systemd.tmpfiles.rules = [
+        "d /var/local/cherryldap 0755 cherryldap users"
+        "d /var/local/cherryldap/settings_local.py 0644 cherryldap users"
+      ];
+
+      # systemd.services.cherryldap = {
+      #   description = "ResDigita FFDN Coin";
+      #   after = [ "network.target" ];
+      #   wantedBy = [ "multi-user.target" ];
+      #   serviceConfig = {
+      #     WorkingDirectory = "/var/local/cherryldap/";
+      #     ExecStart = ''/var/local/cherryldap/venv/bin/gunicorn --env LDAP_ACTIVATE='true' --env='DEFAULT_FROM_EMAIL' --access-logfile /var/log/cherryldap-access.log --error-logfile /var/log/cherryldap-error.log --chdir /var/local/cherryldap --workers 12 --bind 127.0.0.1:8000 lesgv.wsgi:application'';
+      #     Restart = "always";
+      #     RestartSec = "10s";
+      #     User = "wagtail";
+      #     Group = "users";
+      #   };
+      #   unitConfig = {
+      #     StartLimitInterval = "1min";
+      #   };
+      # };
+    };
+  };
+  containers.openldap = {
+    autoStart = true;
+
+    privateNetwork = true;
+    hostAddress = "192.168.107.10";
+    localAddress = "192.168.107.11";
+    hostAddress6 = "fa01::1";
+    localAddress6 = "fa01::2";
+    config = { config, pkgs, lib, ...  }: {
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+      ];
+      system.stateVersion = "24.05";
+      nix.settings.experimental-features = "nix-command flakes";
+      networking = {
+        firewall.allowedTCPPorts = [ 3000 4971 4972 22 25 80 443 143 587 993 995 636 8443 9443 ];
+        useHostResolvConf = true;
+        # useHostResolvConf = lib.mkForce false;
+      };
+      time.timeZone = "Europe/Amsterdam";
     };
   };
   # containers.seafile = {
@@ -1177,7 +1373,46 @@ in
   #       #   allowedTCPPorts = [ 80 443 ];
   #       # };
   #       # Use systemd-resolved inside the container
+  #       useHostResolvConf = lib.mk  # containers.freeipa = {
+  #   autoStart = true;
+
+  #   privateNetwork = true;
+  #   hostAddress = "192.168.107.10";
+  #   localAddress = "192.168.107.11";
+  #   hostAddress6 = "fa01::1";
+  #   localAddress6 = "fa01::2";
+  #   config = { config, pkgs, lib, ...  }: {
+  #     environment.systemPackages = with pkgs; [
+  #       ((vim_configurable.override {  }).customize{
+  #         name = "vim";
+  #         vimrcConfig.customRC = ''
+  #           " your custom vimrc
+  #           set mouse=a
+  #           set nocompatible
+  #           colo torte
+  #           syntax on
+  #           set tabstop     =2
+  #           set softtabstop =2
+  #           set shiftwidth  =2
+  #           set expandtab
+  #           set autoindent
+  #           set smartindent
+  #           " ...
+  #         '';
+  #         }
+  #       )
+  #       freeipa
+  #     ];
+  #     system.stateVersion = "24.05";
+  #     nix.settings.experimental-features = "nix-command flakes";
+  #     networking = {
+  #       firewall.allowedTCPPorts = [ 3000 4971 4972 22 25 80 443 143 587 993 995 636 8443 9443 ];
+  #       # useHostResolvConf = true;
   #       useHostResolvConf = lib.mkForce false;
+  #     };
+  #     time.timeZone = "Europe/Amsterdam";
+  #   };
+  # };Force false;
   #     };
   #     users.users.seafile = {
   #       isNormalUser = true;
@@ -1199,7 +1434,21 @@ in
   #   };
   # };    
 
-  # containers.freeipa = {
+
+
+  # containers.filestash = {
+  #   autoStart = true;
+  #   privateNetwork = true;
+  #   hostAddress = "192.168.101.10";
+  #   localAddress = "192.168.101.11";
+  #   hostAddress6 = "fd00::1";
+  #   localAddress6 = "fd00::2";
+  #   config = { config, pkgs, lib, ...  }: {
+  #     environment.systemPackages = with pkgs; [
+  #       ((vim_configurable.override {  }).customize{
+  #         name = "vim";
+  #         vimrcConfig.customRC = ''
+  #           " your custom vimrc  # containers.freeipa = {
   #   autoStart = true;
 
   #   privateNetwork = true;
@@ -1239,20 +1488,6 @@ in
   #     time.timeZone = "Europe/Amsterdam";
   #   };
   # };
-
-  # containers.filestash = {
-  #   autoStart = true;
-  #   privateNetwork = true;
-  #   hostAddress = "192.168.101.10";
-  #   localAddress = "192.168.101.11";
-  #   hostAddress6 = "fd00::1";
-  #   localAddress6 = "fd00::2";
-  #   config = { config, pkgs, lib, ...  }: {
-  #     environment.systemPackages = with pkgs; [
-  #       ((vim_configurable.override {  }).customize{
-  #         name = "vim";
-  #         vimrcConfig.customRC = ''
-  #           " your custom vimrc
   #           set mouse=a
   #           set nocompatible
   #           colo torte
@@ -1690,162 +1925,4 @@ in
   #     };
   #   };
   # };
-  systemd.tmpfiles.rules = [
-    "d /var/local/cherryldap 0755 cherryldap users"
-  ];
-  users.users.cherryldap = {
-    isNormalUser = true;
-    uid = 11111;
-  };
-  containers.cherryldap = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "192.168.106.1";
-    localAddress = "192.168.106.2";
-    hostAddress6 = "fc00::6:1";
-    localAddress6 = "fc00::6:2"; 
-    bindMounts = { 
-      "/var/local/cherryldap" = { 
-        hostPath = "/var/local/cherryldap";
-        isReadOnly = false; 
-      }; 
-    };
-    config = { config, pkgs, ... }: {
-      nix.settings.experimental-features = "nix-command flakes";
-      time.timeZone = "Europe/Paris";
-      system.stateVersion = "24.05";
-      environment.systemPackages = with pkgs; [
-        ((vim_configurable.override {  }).customize{
-          name = "vim";
-          vimrcConfig.customRC = ''
-            " your custom vimrc
-            set mouse=a
-            set nocompatible
-            colo torte
-            syntax on
-            set tabstop     =2
-            set softtabstop =2
-            set shiftwidth  =2
-            set expandtab
-            set autoindent
-            set smartindent
-            " ...
-          '';
-          }
-        )
-        # python311Packages.cherrypy-cors
-        # python311Packages.pillow
-        # python311Packages.gunicorn
-        # python311Packages.pip
-        # libjpeg
-        # zlib
-        # libtiff
-        # freetype
-        # python311Packages.venvShellHook
-        curl
-        wget
-        lynx
-        libclang
-        # dig    
-        # python311Packages.pylibjpeg-libjpeg
-        git
-        # tmux
-        # bat
-        # cowsay
-        # lzlib
-        # killall
-        # pwgen
-        # python311Packages.pypdf2
-        # python311Packages.pq
-        # python311Packages.aiosasl
-        # python311Packages.psycopg2
-        # gettext
-        # sqlite
-        # postgresql_14
-        # pipx
-        gnumake
-        gcc
-        glibcLocales
-        # python311Packages.manimpango
-        # python311Packages.devtools
-
-        # python311Packages.django-auth-ldap
-        # python311Packages.pq
-        # python311Packages.aiosasl
-        # python311Packages.pylibjpeg-libjpeg
-        # python311Packages.virtualenv
-        # python311Packages.toolz
-        # libpqxx
-        # postgresql
-        openldap
-        # python311Packages.pgcli
-        # cairo
-        # cairomm
-        # python311Packages.pycairo
-        # python311Packages.cairosvg
-        # python311Packages.cairocffi
-
-        gnutls
-        gnulib
-        gnumake
-        openssl
-        cyrus_sasl
-        # ldapcherry
-        (pkgs.python3.withPackages (python-pkgs: [
-          # python-pkgs.devtools
-          # python-pkgs.pydevtool
-          python-pkgs.cherrypy
-          python-pkgs.mako
-          python-pkgs.pyyaml
-          python-pkgs.python-ldap
-          python-pkgs.yq
-          python-pkgs.pip
-          # python-pkgs.python-ldap-test
-          # python-pkgs.ldappool
-          # python-pkgs.wheel
-          # python-pkgs.wheelUnpackHook
-          # python-pkgs.installer
-          # python-pkgs.bootstrap.installer
-          # python-pkgs.pandas
-          # python-pkgs.requests
-        ]))
-
-        ninja
-        cmake
-        # PHP
-        php
-        ];
-      networking = {
-        hostName = "cherryldap"; 
-        firewall.allowedTCPPorts = [ 22 25 53 80 443 143 587 993 995 636 ];
-        useHostResolvConf = lib.mkForce false;
-      };     
-      services.resolved.enable = true;   
-      users.users.cherryldap = {
-        isNormalUser = true;
-        uid = 11111;
-      };
-      systemd.tmpfiles.rules = [
-        "d /var/local/cherryldap 0755 cherryldap users"
-        "d /var/local/cherryldap/settings_local.py 0644 cherryldap users"
-      ];
-
-      # systemd.services.cherryldap = {
-      #   description = "ResDigita FFDN Coin";
-      #   after = [ "network.target" ];
-      #   wantedBy = [ "multi-user.target" ];
-      #   serviceConfig = {
-      #     WorkingDirectory = "/var/local/cherryldap/";
-      #     ExecStart = ''/var/local/cherryldap/venv/bin/gunicorn --env LDAP_ACTIVATE='true' --env='DEFAULT_FROM_EMAIL' --access-logfile /var/log/cherryldap-access.log --error-logfile /var/log/cherryldap-error.log --chdir /var/local/cherryldap --workers 12 --bind 127.0.0.1:8000 lesgv.wsgi:application'';
-      #     Restart = "always";
-      #     RestartSec = "10s";
-      #     User = "wagtail";
-      #     Group = "users";
-      #   };
-      #   unitConfig = {
-      #     StartLimitInterval = "1min";
-      #   };
-      # };
-    };
-  };
 }
