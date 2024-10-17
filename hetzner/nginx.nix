773d3540812fa181be060003ae8c8d022efc5177
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-17 11:01:10 +0200

    Extra EWords

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -120,7 +120,6 @@ in
           locations."/" = {
             proxyPass = "http://localhost:8090";
             # proxyWebsockets = true      locations."/.well-known" = { proxyPass = null; };
-;
             # extraConfig = ''
             # proxy_set_header   X-Real-IP $remote_addr;
             # proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
