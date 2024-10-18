66cee6582f0cac4107e1dd17326eb54ea46adea1
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-18 05:16:46 +0200

    No Mailserver

--- hetzner/containers.nix
+++ hetzner/containers.nix
@@ -1696,90 +1696,90 @@ containers.mailserver = {
         #   chroot = false;
         #   maxproc = 1;
         # };
-        fail2ban = {
-          enable = true;
-          maxretry = 5; # Observe 5 violations before banning an IP
-          ignoreIP = whitelistSubnets;
-          bantime = "24h"; # Set bantime to one day
-          bantime-increment = {
-            enable = true; # Enable increment of bantime after each violation
-            formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-            # multipliers = "1 2 4 8 16 32 64";
-            maxtime = "168h"; # Do not ban for more than 1 week
-            overalljails = true; # Calculate the bantime based on all the violations
-          };
-          jails = {
-            apache-nohome-iptables = ''
-              # Block an IP address if it accesses a non-existent
-              # home directory more than 5 times in 10 minutes,
-              # since that indicates that it's scanning.
-              filter = apache-nohome
-              action = iptables-multiport[name=HTTP, port="http,https"]
-              logpath = /var/log/httpd/error_log*
-              backend = auto
-              findtime = 600
-              bantime  = 600
-              maxretry = 5
-            '';
-            # postfix = ''
-            #   port     = smtp,465,submission,imap,imaps,pop3,pop3s
-            #   action = iptables-multiport[name=HTTP, port="smtp,465,submission,imap,imaps,pop3,pop3s"]
-            #   logpath  = /var/log/postfix.log
-            #   backend  = auto
-            #   enabled  = true
-            #   filter   = postfix[mode=auth]
-            #   mode     = more
-            # '';
-            # dovecot = ''
-            #   port     = smtp,465,submission
-            #   logpath  = /var/log/fail2ban.log
-            #   backend  = auto
-            #   enabled  = true
-            #   mode     = more
-            # '';
-            # postfix-sasl = ''
-            #   filter   = postfix[mode=auth]
-            #   port     = smtp,465,submission,imap,imaps,pop3,pop3s
-            #   logpath  = /var/log/fail2ban.log
-            #   backend  = auto
-            #   enabled  = true
-            #   mode     = more
-            # '';
-          };
-        };
-        roundcube = {
-          enable = true;
-          # this is the url of the vhost, not necessarily the same as the fqdn of
-          # the mailserver
-          hostName = "mail.gv.coop";
-          #  dicts =  [ en fr de ];
-          extraConfig = ''
-              # starttls needed for authentication, so the fqdn required to match
-              # the certificate
-              $config['smtp_server'] = "tls://mail.gv.coop";
-              $config['smtp_user'] = "%u";
-              $config['smtp_pass'] = "%p";
-              # $config['oauth_provider'] = 'generic';
-              # $config['oauth_provider_name'] = 'authentik';
-              # $config['oauth_client_id'] = 'q3nTVQdV2ctY8GeNKvPuHokNa5RxT0VhZbVFCyY3';
-              # $config['oauth_client_secret'] = '${oauthPassword}';
-              # $config['oauth_auth_uri'] = 'https://authentik.resdigita.com/application/o/authorize/';
-              # $config['oauth_token_uri'] = 'https://authentik.resdigita.com/application/o/token/';
-              # $config['oauth_identity_uri'] = 'https://authentik.resdigita.com/application/o/userinfo/';
-              # $config['oauth_scope'] = "openid dovecotprofile email";
-              # $config['oauth_auth_parameters'] = [];
-              # $config['oauth_identity_fields'] = ['email'];
-              $config['generic_message_footer_html'] = '<a href="https://www.gv.coop">Les Grands Voisins .com comme communautés</a>';
-              $config['session_samesite'] = "Lax";
-              $config['support_url'] = 'https://www.gv.coop';
-              $config['product_name'] = 'Roundcube Webmail des GV';
-              $config['session_debug'] = true;
-              $config['session_domain'] = 'mail.gv.coop';
-              $config['login_password_maxlen'] = 4096;
-          '';
-          dicts = [ pkgs.aspellDicts.fr pkgs.aspellDicts.en ];
-          maxAttachmentSize = 75;
-        };
+        # fail2ban = {
+        #   enable = true;
+        #   maxretry = 5; # Observe 5 violations before banning an IP
+        #   ignoreIP = whitelistSubnets;
+        #   bantime = "24h"; # Set bantime to one day
+        #   bantime-increment = {
+        #     enable = true; # Enable increment of bantime after each violation
+        #     formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+        #     # multipliers = "1 2 4 8 16 32 64";
+        #     maxtime = "168h"; # Do not ban for more than 1 week
+        #     overalljails = true; # Calculate the bantime based on all the violations
+        #   };
+        #   jails = {
+        #     apache-nohome-iptables = ''
+        #       # Block an IP address if it accesses a non-existent
+        #       # home directory more than 5 times in 10 minutes,
+        #       # since that indicates that it's scanning.
+        #       filter = apache-nohome
+        #       action = iptables-multiport[name=HTTP, port="http,https"]
+        #       logpath = /var/log/httpd/error_log*
+        #       backend = auto
+        #       findtime = 600
+        #       bantime  = 600
+        #       maxretry = 5
+        #     '';
+        #     postfix = ''
+        #       port     = smtp,465,submission,imap,imaps,pop3,pop3s
+        #       action = iptables-multiport[name=HTTP, port="smtp,465,submission,imap,imaps,pop3,pop3s"]
+        #       logpath  = /var/log/postfix.log
+        #       backend  = auto
+        #       enabled  = true
+        #       filter   = postfix[mode=auth]
+        #       mode     = more
+        #     '';
+        #     # dovecot = ''
+        #     #   port     = smtp,465,submission
+        #     #   logpath  = /var/log/fail2ban.log
+        #     #   backend  = auto
+        #     #   enabled  = true
+        #     #   mode     = more
+        #     # '';
+        #     # postfix-sasl = ''
+        #     #   filter   = postfix[mode=auth]
+        #     #   port     = smtp,465,submission,imap,imaps,pop3,pop3s
+        #     #   logpath  = /var/log/fail2ban.log
+        #     #   backend  = auto
+        #     #   enabled  = true
+        #     #   mode     = more
+        #     # '';
+        #   };
+        # };
+        # roundcube = {
+        #   enable = true;
+        #   # this is the url of the vhost, not necessarily the same as the fqdn of
+        #   # the mailserver
+        #   hostName = "mail.gv.coop";
+        #   #  dicts =  [ en fr de ];
+        #   extraConfig = ''
+        #       # starttls needed for authentication, so the fqdn required to match
+        #       # the certificate
+        #       $config['smtp_server'] = "tls://mail.gv.coop";
+        #       $config['smtp_user'] = "%u";
+        #       $config['smtp_pass'] = "%p";
+        #       # $config['oauth_provider'] = 'generic';
+        #       # $config['oauth_provider_name'] = 'authentik';
+        #       # $config['oauth_client_id'] = 'q3nTVQdV2ctY8GeNKvPuHokNa5RxT0VhZbVFCyY3';
+        #       # $config['oauth_client_secret'] = '${oauthPassword}';
+        #       # $config['oauth_auth_uri'] = 'https://authentik.resdigita.com/application/o/authorize/';
+        #       # $config['oauth_token_uri'] = 'https://authentik.resdigita.com/application/o/token/';
+        #       # $config['oauth_identity_uri'] = 'https://authentik.resdigita.com/application/o/userinfo/';
+        #       # $config['oauth_scope'] = "openid dovecotprofile email";
+        #       # $config['oauth_auth_parameters'] = [];
+        #       # $config['oauth_identity_fields'] = ['email'];
+        #       $config['generic_message_footer_html'] = '<a href="https://www.gv.coop">Les Grands Voisins .com comme communautés</a>';
+        #       $config['session_samesite'] = "Lax";
+        #       $config['support_url'] = 'https://www.gv.coop';
+        #       $config['product_name'] = 'Roundcube Webmail des GV';
+        #       $config['session_debug'] = true;
+        #       $config['session_domain'] = 'mail.gv.coop';
+        #       $config['login_password_maxlen'] = 4096;
+        #   '';
+        #   dicts = [ pkgs.aspellDicts.fr pkgs.aspellDicts.en ];
+        #   maxAttachmentSize = 75;
+        # };
         memcached = {
           enable = true;
           # maxMemory = 256;
