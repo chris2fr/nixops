dd4137d1ba8b37b38684c31ed6107b3bdfa73375
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-18 15:31:54 +0200

    More ports

--- id.gv.coop/configuration.nix
+++ id.gv.coop/configuration.nix
@@ -101,7 +101,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 443 25 587 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
