83a31649026c031a6eeb817a5ae75a721febb07d
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-18 09:17:13 +0200

    LDAP connectivity

--- hetzner/mailserver.nix
+++ hetzner/mailserver.nix
@@ -135,15 +135,15 @@ in
         passwordFile = "/etc/nixos/.secrets.bind";
       };
       uris = [
-        "ldap:///"
-        # "ldaps://ldap.gv.coop:10636/"
+        # "ldap:///"
+        "ldaps://ldap.gv.coop:10636/"
         # "ldap://ldap.gv.coop:10389/"
       ];
       searchBase = "ou=users,${ldapBaseDCDN}";
       searchScope = "sub";
-      tlsCAFile = "/var/lib/acme/${domainName}/fullchain.pem";
-      # tlsCAFile = "/var/lib/acme/ldap.gv.coop/fullchain.pem";
-      startTls = true;
+      # tlsCAFile = "/var/lib/acme/${domainName}/fullchain.pem";
+      tlsCAFile = "/var/lib/acme/ldap.gv.coop/fullchain.pem";
+      startTls = false;
       postfix = {
         mailAttribute = "mail";
         uidAttribute = "uid";
