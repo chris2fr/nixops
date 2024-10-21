c134bc3eca6500f5bb228ca47c382cb80ec94eef
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-21 08:41:14 +0200

    Just install keycloak without container

--- id.gv.coop/configuration.nix
+++ id.gv.coop/configuration.nix
@@ -148,20 +148,41 @@
 
   # Enable the OpenSSH daemon.
   services = {
-    postgresql = {
+    keycloak = {
       enable = true;
-      enableTCPIP = true;
-      package = pkgs.postgresql_14;
-      ensureDatabases = [
-        "keycloak"
-      ];
-      ensureUsers = [
-        {
-          name = "keycloak";
-          ensureDBOwnership = true;
-        }
-      ]; 
+      database = {
+        type = "postgresql";
+        createLocally = true;
+        username = "keycloak";
+        passwordFile = "/var/run/.secret/.keycloak";
+        # useSSL = false;
+      };
+      settings = {
+        https-port = 12443;
+        http-port = 12080;
+        proxy = "passthrough";
+        # proxy = "reencrypt";
+        hostname = "key.gv.coop";
+        http-enabled = true;
+      };
+      # sslCertificate = "/var/lib/acme/key.gv.coop/fullchain.pem";
+      # sslCertificateKey = "/var/lib/acme/key.gv.coop/key.pem";
+      # themes = {lesgv = (pkgs.callPackage "/etc/nixos/keycloaktheme/derivation.nix" {});};
     };
+    # postgresql = {
+    #   enable = true;
+    #   enableTCPIP = true;
+    #   package = pkgs.postgresql_14;
+    #   ensureDatabases = [
+    #     "keycloak"
+    #   ];
+    #   ensureUsers = [
+    #     {
+    #       name = "keycloak";
+    #       ensureDBOwnership = true;
+    #     }
+    #   ]; 
+    # };
     # tomcat = {
     #   enable = true;
 
@@ -326,102 +347,86 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
-  containers.key = {
-    bindMounts = {
-      "/var/lib/acme/key.gv.coop/" = {
-        hostPath = "/var/lib/acme/key.gv.coop/";
-        isReadOnly = true;
-      }; 
-    };
-    autoStart = true;
-    # privateNetwork = true;
-    # hostAddress = "192.168.105.10";
-    # localAddress = "192.168.105.11";
-    # hostAddress6 = "fa01::1";
-    # localAddress6 = "fa01::2";
-    config = { config, pkgs, lib, ...  }: {
-      environment.systemPackages = with pkgs; [
-        ((vim_configurable.override {  }).customize{
-          name = "vim";
-          vimrcConfig.customRC = ''
-            " your custom vimrc
-            set mouse=a
-            set nocompatible
-            colo torte
-            syntax on
-            set tabstop     =2
-            set softtabstop =2
-            set shiftwidth  =2
-            set expandtab
-            set autoindent
-            set smartindent
-            " ...
-          '';
-          }
-        )
-        git
-        lynx
-        openldap
-      ];
-      # virtualisation.docker.enable = true;
-      system.stateVersion = "24.05";
-      nix.settings.experimental-features = "nix-command flakes";
-      networking = {
-        firewall = {
-          enable = false;
-          allowedTCPPorts = [  443 587 12443 ]; 
-        };
-        useHostResolvConf = lib.mkForce false;
-      };
-      systemd.tmpfiles.rules = [
-        "d /var/run/.secret 0755 root nginx"
-        "f /var/run/.secret/.keycloak 0644 root root"
-        # "f /var/run/.secret/.keycloakdata 0666 root root"
-      ];
-      # security.acme.acceptTerms = true;
-      users = {
-        groups = {
-          "acme" = {
-            gid = 993;
-            members = ["acme"];
-          };
-          "wwwrun" = {
-            gid = 54;
-            members = ["acme" "wwwrun"];
-          };
-        };
-        users = {
-          "acme" = {
-            uid = 994;
-            group = "acme";
-          };
-          "wwwrun" = {
-            uid = 54;
-            group = "wwwrun";
-          };
-        };
-      };
-      services = {
-        resolved.enable = true;
-        keycloak = {
-          enable = true;
-          database = {
-            passwordFile = "/var/run/.secret/.keycloak";
-            # useSSL = false;
-          };
-          settings = {
-            https-port = 12443;
-            http-port = 12080;
-            # proxy = "passthrough";
-            proxy = "reencrypt";
-            hostname = "key.gv.coop";
-          };
-          sslCertificate = "/var/lib/acme/key.gv.coop/fullchain.pem";
-          sslCertificateKey = "/var/lib/acme/key.gv.coop/key.pem";
-          # themes = {lesgv = (pkgs.callPackage "/etc/nixos/keycloaktheme/derivation.nix" {});};
-        };
-      };
-    };
-  };
-}
+#   containers.key = {
+#     bindMounts = {
+#       "/var/lib/acme/key.gv.coop/" = {
+#         hostPath = "/var/lib/acme/key.gv.coop/";
+#         isReadOnly = true;
+#       }; 
+#     };
+#     autoStart = true;
+#     # privateNetwork = true;
+#     # hostAddress = "192.168.105.10";
+#     # localAddress = "192.168.105.11";
+#     # hostAddress6 = "fa01::1";
+#     # localAddress6 = "fa01::2";
+#     config = { config, pkgs, lib, ...  }: {
+#       environment.systemPackages = with pkgs; [
+#         ((vim_configurable.override {  }).customize{
+#           name = "vim";
+#           vimrcConfig.customRC = ''
+#             " your custom vimrc
+#             set mouse=a
+#             set nocompatible
+#             colo torte
+#             syntax on
+#             set tabstop     =2
+#             set softtabstop =2
+#             set shiftwidth  =2
+#             set expandtab
+#             set autoindent
+#             set smartindent
+#             " ...
+#           '';
+#           }
+#         )
+#         git
+#         lynx
+#         openldap
+#       ];
+#       # virtualisation.docker.enable = true;
+#       system.stateVersion = "24.05";
+#       nix.settings.experimental-features = "nix-command flakes";
+#       networking = {
+#         firewall = {
+#           enable = false;
+#           allowedTCPPorts = [  443 587 12443 ]; 
+#         };
+#         useHostResolvConf = lib.mkForce false;
+#       };
+#       systemd.tmpfiles.rules = [
+#         "d /var/run/.secret 0755 root nginx"
+#         "f /var/run/.secret/.keycloak 0644 root root"
+#         # "f /var/run/.secret/.keycloakdata 0666 root root"
+#       ];
+#       # security.acme.acceptTerms = true;
+#       users = {
+#         groups = {
+#           "acme" = {
+#             gid = 993;
+#             members = ["acme"];
+#           };
+#           "wwwrun" = {
+#             gid = 54;
+#             members = ["acme" "wwwrun"];
+#           };
+#         };
+#         users = {
+#           "acme" = {
+#             uid = 994;
+#             group = "acme";
+#           };
+#           "wwwrun" = {
+#             uid = 54;
+#             group = "wwwrun";
+#           };
+#         };
+#       };
+#       services = {
+#         resolved.enable = true;
+        
+#       };
+#     };
+#   };
+# }
 
