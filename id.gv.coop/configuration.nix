64abb2a22525d1330187719668d89d463b298d03
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-21 07:45:15 +0200

    keycloak

--- id.gv.coop/configuration.nix
+++ id.gv.coop/configuration.nix
@@ -209,6 +209,25 @@
         };
       };
       virtualHosts = {
+        "key.gv.coop" = {
+        enableACME = true;
+        forceSSL = true;
+        root = "/var/www/keycloakvillagengo";
+
+        # globalRedirect = "key.gv.coop:12443";
+        locations."/" = {
+          proxyPass = "https://key.gv.coop:12443";
+          extraConfig = ''
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Host $host;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          add_header Content-Security-Policy "frame-src *; frame-ancestors *; object-src *;";
+          add_header Access-Control-Allow-Credentials true;
+          '';
+        };
+      };
         "lemonldap.gv.coop" = {
           enableACME = true; 
           forceSSL = true; 
@@ -294,6 +313,100 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
-
+  containers.keycloak = {
+    bindMounts = {
+      "/var/lib/acme/key.gv.coop/" = {
+        hostPath = "/var/lib/acme/key.gv.coop/";
+        isReadOnly = true;
+      }; 
+    };
+    autoStart = true;
+    # privateNetwork = true;
+    # hostAddress = "192.168.105.10";
+    # localAddress = "192.168.105.11";
+    # hostAddress6 = "fa01::1";
+    # localAddress6 = "fa01::2";
+    config = { config, pkgs, lib, ...  }: {
+      environment.systemPackages = with pkgs; [
+        ((vim_configurable.override {  }).customize{
+          name = "vim";
+          vimrcConfig.customRC = ''
+            " your custom vimrc
+            set mouse=a
+            set nocompatible
+            colo torte
+            syntax on
+            set tabstop     =2
+            set softtabstop =2
+            set shiftwidth  =2
+            set expandtab
+            set autoindent
+            set smartindent
+            " ...
+          '';
+          }
+        )
+        git
+        lynx
+        openldap
+      ];
+      # virtualisation.docker.enable = true;
+      system.stateVersion = "24.05";
+      nix.settings.experimental-features = "nix-command flakes";
+      networking = {
+        firewall = {
+          enable = false;
+          allowedTCPPorts = [  443 587 12443 ]; 
+        };
+        useHostResolvConf = lib.mkForce false;
+      };
+      systemd.tmpfiles.rules = [
+       "f /etc/.secret.keycloakdata 0660 root root"
+      ];
+      # security.acme.acceptTerms = true;
+      users = {
+        groups = {
+          "acme" = {
+            gid = 993;
+            members = ["acme"];
+          };
+          "wwwrun" = {
+            gid = 54;
+            members = ["acme" "wwwrun"];
+          };
+        };
+        users = {
+          "acme" = {
+            uid = 994;
+            group = "acme";
+          };
+          "wwwrun" = {
+            uid = 54;
+            group = "wwwrun";
+          };
+        };
+      };
+      services = {
+        resolved.enable = true;
+        keycloak = {
+          enable = true;
+          database = {
+            passwordFile = "/etc/.secrets.keycloak";
+            # useSSL = false;
+          };
+          settings = {
+            https-port = 12443;
+            http-port = 12080;
+            # proxy = "passthrough";
+            proxy = "reencrypt";
+            hostname = "key.gv.coop";
+          };
+          sslCertificate = "/var/lib/acme/key.gv.coop/fullchain.pem";
+          sslCertificateKey = "/var/lib/acme/key.gv.coop/key.pem";
+          # themes = {lesgv = (pkgs.callPackage "/etc/nixos/keycloaktheme/derivation.nix" {});};
+        };
+      };
+    };
+  };
 }
 
