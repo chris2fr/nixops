3e7016b48676b37831bb7bc355573b0a56269bbb
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-21 11:15:30 +0200

    link.gv.

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -81,7 +81,7 @@ in
           locations."/.well-known" = { proxyPass = null; };
           locations."/" = {
             extraConfig = ''
-            rewrite ^/$ https://link.gv.coop/api/v1/auth/signin/keycloak? redirect;
+            rewrite ^/login$ https://link.gv.coop/api/v1/auth/signin/keycloak? redirect;
             proxy_set_header   X-Real-IP $remote_addr;
             proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header   Host $host;
