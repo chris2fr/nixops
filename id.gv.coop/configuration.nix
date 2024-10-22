7ce8d31ec5abd8b74d79c41f587e6451af7e1624
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-22 06:34:20 +0200

    /admin/serverinfo

--- id.gv.coop/configuration.nix
+++ id.gv.coop/configuration.nix
@@ -266,22 +266,28 @@ in
         forceSSL = true;
         root = "/var/www/key";
         locations = {
+          "/realms/master/account" = {
+            proxyPass = "https://key.gv.coop:12443";
+            extraConfig = extraConfigNginxKeycloak + ''
+              error_page 403 =302 https://key.gv.coop/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.gv.coop%2Frealms%2Fmaster%2Faccount%2F;
+            '';
+          };
+          "/admin/master/console" = {
+            proxyPass = "https://key.gv.coop:12443";
+            extraConfig = extraConfigNginxKeycloak + ''
+              error_page 403 =302 https://key.gv.coop/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.gv.coop%2Frealms%2Fmaster%2Faccount%2F;
+            '';
+          };
+          "/admin/serverinfo" = {
+            proxyPass = "https://key.gv.coop:12443";
+            extraConfig = extraConfigNginxKeycloak + ''
+              error_page 403 =302 https://key.gv.coop/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.gv.coop%2Frealms%2Fmaster%2Faccount%2F;
+            '';
+          };
           "/" = {
             proxyPass = "https://key.gv.coop:12443";
             extraConfig = extraConfigNginxKeycloak + ''
               rewrite ^/$ https://key.gv.coop/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.gv.coop%2Frealms%2Fmaster%2Faccount%2F redirect;
-            '';
-          };
-          "/realms/master/account/" = {
-            proxyPass = "https://key.gv.coop:12443";
-            extraConfig = extraConfigNginxKeycloak + ''
-              error_page 403 =302 https://key.gv.coop/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.gv.coop%2Frealms%2Fmaster%2Faccount%2F;
-            '';
-          };
-          "/admin/master/console/" = {
-            proxyPass = "https://key.gv.coop:12443";
-            extraConfig = extraConfigNginxKeycloak + ''
-              error_page 403 =302 https://key.gv.coop/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.gv.coop%2Frealms%2Fmaster%2Faccount%2F;
             '';
           };
         };
