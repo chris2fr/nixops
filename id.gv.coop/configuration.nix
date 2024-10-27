f0ef10a0598b25f94c67a57ff3ea6bffdc631784
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-27 19:49:53 +0100

    builtins

--- id.gv.coop/configuration.nix
+++ id.gv.coop/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 # inputs.docspell.url = "github:eikek/docspell?dir=nix/";
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, builtins, ... }:
 let
   extraConfigNginxKeycloak = ''
     proxy_set_header Host $host;
