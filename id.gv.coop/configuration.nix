6af88a850ff061e66dd5cd9806de191f96454bab
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-27 19:45:07 +0100

    systeme

--- id.gv.coop/configuration.nix
+++ id.gv.coop/configuration.nix
@@ -23,6 +23,7 @@ let
 in
 {
   nix.settings.experimental-features = "nix-command flakes";
+  system.stateVersion = "24.05";
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -408,7 +409,7 @@ in
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+  # system.stateVersion = "24.05"; # Did you read the comment?
 #   containers.key = {
 #     bindMounts = {
 #       "/var/lib/acme/key.gv.coop/" = {
