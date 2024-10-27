c16ad8732207eed0a37f680be65ff82a0f9e907c
Chris Mann <chris@lesgtrandsvoisins.com>
2024-10-27 05:49:18 +0100

    #

--- id.gv.coop/configuration.nix
+++ id.gv.coop/configuration.nix
@@ -283,7 +283,7 @@ in
             "/admin/master/console" = {
               # proxyPass = "https://key.lesgrandsvoisins.com:12443";
               proxyPass = "https://key.gv.coop:12443";
-              # extraConfig = extraConfigNginxKeycloak + ''
+              extraConfig = extraConfigNginxKeycloak + ''
                 # error_page 403 =302 https://key.lesgrandsvoisins.com/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.lesgrandsvoisins.com%2Frealms%2Fmaster%2Faccount%2F;
                 error_page 403 =302 https://key.gv.coop/realms/master/protocol/openid-connect/auth?client_id=account-console&redirect_uri=https%3A%2F%2Fkey.gv.coop%2Frealms%2Fmaster%2Faccount%2F;
               '';
