3169d8b02cb05cc07d1cfc3bf1eb1dbbae44c9cf
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-13 11:28:25 +0100

    Using SFTPGO Auth from ./etc/sftp/

--- hetzner/etc/sftpgo/sftpgoPluginAuth0.nix
+++ hetzner/etc/sftpgo/sftpgoPluginAuth0.nix
@@ -1,14 +1,13 @@
 {
-  pkgs ? import (fetchurl {
-    url = "https://github.com/sftpgo/sftpgo-plugin-auth/releases/download/v1.0.9/sftpgo-plugin-auth-linux-amd64";
-    sha256 = "sha256-xLmFTH6PcNMF73zmhCHalIv6zF+OmwH9GAKEXOXfGdM=";
-  }) {}
+  stdenv,
+  fetchurl,
 }:
-pkgs.stdenv.mkDerivation rec {
+
+stdenv.mkDerivation {
   pname = "sftpgo-plugin-auth";
   version = "1.0.9";
 
-  src = pkgs.fetchgit {
+  src = fetchurl {
     url = "https://github.com/sftpgo/sftpgo-plugin-auth/releases/download/v1.0.9/sftpgo-plugin-auth-linux-amd64";
     sha256 = "sha256-xLmFTH6PcNMF73zmhCHalIv6zF+OmwH9GAKEXOXfGdM=";
   };
