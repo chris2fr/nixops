56c28afb0fbcdbf0e73d345bdbfc3b73384631e7
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-13 13:07:55 +0100

    Secret management

--- hetzner/systemd.nix
+++ hetzner/systemd.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 let 
   mannchriRsaPublic = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuBWybYSoR6wyd1EG5YnHPaMKE3RQufrK7ycej7avw3Ug8w8Ppx2BgRGNR6EamJUPnHEHfN7ZZCKbrAnuP3ar8mKD7wqB2MxVqhSWvElkwwurlijgKiegYcdDXP0JjypzC7M73Cus3sZT+LgiUp97d6p3fYYOIG7cx19TEKfNzr1zHPeTYPAt5a1Kkb663gCWEfSNuRjD2OKwueeNebbNN/OzFSZMzjT7wBbxLb33QnpW05nXlLhwpfmZ/CVDNCsjVD1+NXWWmQtpRCzETL6uOgirhbXYW8UyihsnvNX8acMSYTT9AA3jpJRrUEMum2VizCkKh7bz87x7gsdA4wF0/w== rsa-key-20220407";
-  newuserPW = import "/etc/nixos/.secrets.newuser";
+  newuserPW = (lib.removeSuffix "\n" (builtins.readFile /etc/nixos/.secrets.newuser));
 in
 {
   systemd.services = {
