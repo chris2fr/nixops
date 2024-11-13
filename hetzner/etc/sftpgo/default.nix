89c4b25e17a74eef7ec3bba0c23b0c72ed7ec61c
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-13 11:47:09 +0100

    Using SFTPGO Auth from ./etc/sftp/sftpgoPluginAuth

--- hetzner/etc/sftpgo/default.nix
+++
@@ -1,7 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
-in
-{
-  sftpgoPluginAuth = pkgs.callPackage ./sftpgoPluginAuth.nix { };
-}
