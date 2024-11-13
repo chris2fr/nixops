920f39bc57946428143cf6326b957a96359066d4
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-13 12:08:43 +0100

    Added a service

--- hetzner/systemd.nix
+++ hetzner/systemd.nix
@@ -3,7 +3,18 @@ let
   mannchriRsaPublic = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAuBWybYSoR6wyd1EG5YnHPaMKE3RQufrK7ycej7avw3Ug8w8Ppx2BgRGNR6EamJUPnHEHfN7ZZCKbrAnuP3ar8mKD7wqB2MxVqhSWvElkwwurlijgKiegYcdDXP0JjypzC7M73Cus3sZT+LgiUp97d6p3fYYOIG7cx19TEKfNzr1zHPeTYPAt5a1Kkb663gCWEfSNuRjD2OKwueeNebbNN/OzFSZMzjT7wBbxLb33QnpW05nXlLhwpfmZ/CVDNCsjVD1+NXWWmQtpRCzETL6uOgirhbXYW8UyihsnvNX8acMSYTT9AA3jpJRrUEMum2VizCkKh7bz87x7gsdA4wF0/w== rsa-key-20220407";
 in
 {
-    systemd.services = {
+  systemd.services = {
+    sftpgo = {
+      enable = true;
+      wantedBy = ["default.target"];
+      script = "${pkgs.sftpgo}/bin/sftpgo serve --config-file sftpgo.json";
+      description = "SFTPGo for drive features of Les Grands Voisins";
+      serviceConfig = {
+        WorkingDirectory = "/home/mannchri/sftpgo/nix/";
+        User = "mannchri";
+        Group = "users";
+      };
+    };
     "filebrowser@" = {
       enable = true;
       wantedBy = ["default.target"];
