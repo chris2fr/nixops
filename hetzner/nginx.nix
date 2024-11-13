aba6ddd3202629742e67a88ffd194ada40c0b693
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-13 14:09:49 +0100

    Executable of sftpgo-plugin-auth from custom derivation

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -457,9 +457,12 @@ in
           locations."/".proxyPass = "http://127.0.0.1:8088";
           extraConfig = ''
             client_max_body_size 500M;
-            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_http_version 1.1;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Real-IP $remote_addr;
             proxy_redirect off;
+            proxy_set_header Host $host;
           '';
         };
         "minio.lesgrandsvoisins.com" = {
