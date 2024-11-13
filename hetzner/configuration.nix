3ebbb5afaa1025b89f30e0c3f0c51b12238e33b5
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-13 18:22:36 +0100

    Username

--- hetzner/configuration.nix
+++ hetzner/configuration.nix
@@ -448,6 +448,7 @@ in
               config_url = "https://key.lesgrandsvoisins.com/realms/master/";
               client_id = "sftpgo";
               client_secret = keySftpgo;
+              username = "username";
             };
           }
           {
