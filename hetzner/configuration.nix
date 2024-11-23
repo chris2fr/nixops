0d641e8d6966e9b4c3acc2ef2e8268e021fa00d4
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-23 12:05:05 +0100

    Restoring SFTPGO

--- hetzner/configuration.nix
+++ hetzner/configuration.nix
@@ -427,6 +427,120 @@ in
         };
       };
     };
+    sftpgo = {
+      enable = true;
+      user = "sftpgo";  
+      group = "wwwrun";
+      dataDir = "/var/www/dav/data";
+      extraArgs = [
+          "--log-level"
+          "info"
+        ];
+      settings = {
+        webdavd.bindings = [
+          {
+            port = 14443;
+            address = "116.202.236.241";
+            certificate_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/full.pem";
+            certificate_key_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/key.pem";
+            enable_https = true;
+          }
+          {
+            port = 14443;
+            address = "[2a01:4f8:241:4faa::]";
+            certificate_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/full.pem";
+            certificate_key_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/key.pem";
+            enable_https = true;
+          }
+        ];
+        sftpd.bindings = [
+          {
+            port = 2022;
+            address = "116.202.236.241";
+          }
+          {
+            port = 2022;
+            address = "[2a01:4f8:241:4faa::]";
+          }
+        ];
+        httpd = {
+          static_files_path = "/var/run/sftpgo/static";
+          templates_path = "/var/run/sftpgo/templates";
+          bindings = [
+          {
+            port = 10443;
+            address = "116.202.236.241";
+            certificate_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/full.pem";
+            certificate_key_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/key.pem";
+            enable_https = true;
+            # oidc = {
+            #   config_url = "https://key.lesgrandsvoisins.com/realms/master";
+            #   client_id = "sftpgo";
+            #   client_secret = keySftpgo;
+            #   username_field = "username";
+            #   redirect_base_url = "https://sftpgo.lesgrandsvoisins.com:10443";
+            #   scopes = [
+            #     "openid"
+            #     "profile"
+            #     "email"        
+            #   ];
+            #   implicit_roles = true;
+            # };
+            branding = {
+              web_admin = {
+                name = "sftpgo.lesgrandsovisins.com : Accès Administrateur au Drive des Grands Voisins";
+                short_name = "ADMIN du Drive des Grands Voisins";
+              };
+              web_client = {
+                name = "sftpgo.lesgrandsovisins.com : Accès au Drive des Grands Voisins";
+                short_name = "Drive des Grands Voisins";
+              };
+            };
+          }
+          {
+            port = 10443;
+            address = "[2a01:4f8:241:4faa::]";
+            certificate_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/full.pem";
+            certificate_key_file = "/var/lib/acme/sftpgo.lesgrandsvoisins.com/key.pem";
+            enable_https = true;
+            # oidc = {
+            #   config_url = "https://key.lesgrandsvoisins.com/realms/master";
+            #   client_id = "sftpgo";
+            #   client_secret = keySftpgo;
+            #   redirect_base_url = "https://sftpgo.lesgrandsvoisins.com:10443";
+            #   username_field = "username";
+            #   scopes = [
+            #     "openid"
+            #     "profile"
+            #     "email"
+            #   ];
+            #   implicit_roles = true;
+            # };
+            branding = {
+              web_admin = {
+                name = "sftpgo.lesgrandsovisins.com : Accès Administrateur au Drive des Grands Voisins";
+                short_name = "ADMIN du Drive des Grands Voisins";
+              };
+              web_client = {
+                name = "sftpgo.lesgrandsovisins.com : Accès au Drive des Grands Voisins";
+                short_name = "Drive des Grands Voisins";
+              };
+            };
+          }
+        ];
+        };
+        plugins = [{
+          type = "auth";
+          cmd = "/run/current-system/sw/bin/sftpgo-plugin-auth";
+          args = ["serve"
+            "--config-file"
+            "/var/run/sftpgo/sftpgo-plugin-auth.json"
+          ];
+          auth_options.scope = 5;
+          auto_mtls = true;
+        }];
+      };  
+    };
     minio = {
       enable = true;
     };
