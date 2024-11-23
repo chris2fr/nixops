9da4756c0fdd189f9e4c714057f51da90dd89982
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-23 16:14:58 +0100

    Limiting the ports

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -35,7 +35,8 @@ in
       recommendedTlsSettings = true;
       recommendedProxySettings = true;
 
-      defaultListenAddresses = [ "0.0.0.0" "116.202.236.241" "[::]" "[::1]"];
+      defaultListenAddresses = [ "127.0.0.1" "116.202.236.241" "[2a01:4f8:241:4faa::]" "[2a01:4f8:241:4faa::1]" "[::1]"];
+      # defaultListenAddresses = [ "0.0.0.0" "116.202.236.241" "[::]" "[::1]"];
       #defaultListen = [{ addr = "0.0.0.0"; port=8888; } { addr = "[::]"; port=8443; } { addr="[2a01:4f8:241:4faa::100]" ; port=443;} ];
       appendHttpConfig = ''
         proxy_headers_hash_max_size 4096;
