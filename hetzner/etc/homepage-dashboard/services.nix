26807822272d18d3203ea4fbaf57cb0265727bef
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-23 18:55:20 +0100

    Services

---
+++ hetzner/etc/homepage-dashboard/services.nix
@@ -0,0 +1,195 @@
+[
+  {
+    "Services GV unifiés stables" = [
+      {
+        "Keycloak pour la connexion unifiée" = {
+          href = "https://keycloak.resdigita.com/realms/master/account";
+          description = "Serveur de connexion et déconnexion unifiées (SSO en OAuth2) pour plusieurs services GV.";
+          noticon = "https://keycloak.resdigita.com/resources/p9m9x/account/keycloak.v2/public/logo.svg";
+          icon = "https://avatars.githubusercontent.com/u/4921466";
+        }
+      }
+      {
+        "HedgeDoc carnets collaboratifs en markdown" = {
+          href = "https://hedgedoc.resdigita.com/auth/oauth2";
+          noticon = "https://hedgedoc.resdigita.com/banner/banner_vertical_color.svg";
+          icon = "https://hedgedoc.resdigita.com/icons/android-chrome-512x512.png";
+          description = "Un serveur de documents en format Markdown pouvant être modifiés par plusieurs personnes en même temps.";
+        }
+      }
+    ]
+  };
+  {
+    "Ressasie de login GV" = [
+      {
+        "Guichet du profil et du mot de passe" = {
+          href = "https://guichet.resdigita.com/user";
+          icon = "https://www.lesgrandsvoisins.com/media/images/gv.original.svg";
+          description = "Depuis Guichet, vous pouvez ajouter et supprimer des boîtes-aux-lettres de courriel.";
+        }
+      }
+      {
+        "Roundcube Webmail" = {
+          href = "https://mail.lesgrandsvoisins.com";
+          description = "Consulter vos courriels des comptes des GV avec pour login le courriel du compte GV et le mot de passe de votre compte des GV.";
+          icon = "https://mail.lesgrandsvoisins.com/skins/elastic/images/logo.svg?s=1";
+        }
+      }
+    ]
+  };
+  {
+    "Services à accès ouverts stables" = [
+      {
+        "BigBlueButton pour les réunions visio et formation" = {
+          href = "https://meet.lesgrandsvoisins.com/";
+          description = "Un espace de rencontres visio qui tient vraiement la route.";
+          icon = "https://bigbluebutton.org/wp-content/uploads/2021/01/BigBlueButton_icon.svg.png";
+        }
+      }
+      {
+        "Uptime-Kuma pour les statuts des sites essentiels" = {
+          href = "https://uptime-kuma.resdigita.com/status/gv";
+          description = "Uptime-Kuma vérifie le statut de nos services. Cette page montre l'état de certains services essentiels.";
+          icon = "https://uptime.kuma.pet/img/icon.svg";
+        }
+      }
+    ]
+  };
+  {
+    "Sites webs publics" = [
+      {
+        "LesGrandsVoisins.fr est notre portail" = {
+          href = "https://www.lesgrandsvoisins.com/";
+          description = "Le site internet des GV.";
+          icon = "https://www.lesgrandsvoisins.com/media/images/gv.original.svg";
+        }
+      }
+      {
+        "Blog.LesGrandsVoisisns.com pour les nouvelles" = {
+          href = "https://blog.lesgrandsvoisins.com";
+          icon = "https://blog.lesgrandsvoisins.com/ghost/assets/img/apple-touch-icon-74680e326a7e87b159d366c7d4fb3d4b.png";
+          description = "Le blog des Grands Voisins.";
+        }
+      }
+      {
+        "Quartz.ResDigita.com pour la doc technique" = {
+          href = "https://quartz.resdigita.com/";
+          icon = "https://quartz.jzhao.xyz/static/icon.png";
+          description = "Documentation de Resdigita des GV sur l'ensemble de nos services autonomes de par et pour les GV";
+        }
+      }
+    ]
+  };
+  {
+    "Services GV unifiés en bêta" = [
+      {
+        "Vikunja gestionnaire des tâches à faire en équipe" = {
+          href = "https://vikunja.resdigita.com";
+          icon = "https://vikunja.resdigita.com/assets/llama-716c88d0.svg?url";
+          description = "Vikunja nous permet de gérer les tâches à faire dans nos équipes.";
+        }
+      }
+    ]
+  };
+  {
+    "Services à accès ouverts en bêta" = [
+      {
+        "VaultWarden pour les maux des mots de passe compatible BitWarden" = {
+          href = "https://vaultwarden.resdigita.com";
+          description = "VaultWarden est un système de gestion de mots de passe. Tout client BitWarden (Linux, MacOS, Windows, iOS, Android, FDroid) fonctionne à condition d'utiliser le serveur auto-hébergé https://vaultwarden.resdigita.com.";
+          icon = "https://vaultwarden.resdigita.com/images/apple-touch-icon.png";
+        }
+      }
+      {
+        "EtherCalc pour un tableur collaboratif" = {
+          href = "https://ethercalc.resdigita.com";
+          icon = "https://ethercalc.resdigita.com/static/img/davy/gfx/home-logo2.png";
+          description = "Créer des tableurs pouvant être modifiés en temps réel par plusieurs personnes.";
+        }
+      }
+      {
+        "Crabfit pour trouver un moment de rendez-vous" = {
+          href = "https://crabfit.resdigita.com/";
+          icon = "https://crab.fit/_next/static/media/logo.b29ef579.svg";
+          description = "Indiquer d'abord votre nom, puis choisir les moments où vous êtes possiblement disponibles et c'est tout.";
+        }
+      }
+    ]
+  };
+  {
+    "Comptes indépendants" = [
+      {
+        "Wagtail.resdigita.com pour la gestion du contenu des sites webs" = {
+          href = "https://wagtail.resdigita.com/admin/";
+          description = "Gestion du contenu du site web";
+          icon = "https://www.lesgrandsvoisins.com/static/wagtailadmin/images/favicon.28de7d61be60.ico";
+        }
+      }
+      {
+        "Ghost pour la gestion du blog.lesgrandsvoisins.com" = {
+          href = "https://blog.lesgrandsvoisins.com/ghost/";
+          description = "Gestion du contenu du site blog";
+          icon = "https://blog.lesgrandsvoisins.com/ghost/assets/img/apple-touch-icon-74680e326a7e87b159d366c7d4fb3d4b.png";
+        }
+      }
+      {
+        "ListMonk pour les listes de diffusion" = {
+          href = "https://list.lesgrandsvoisins.com";
+          description = "Le serveur de listes de diffusion des GV.";
+          noticon = "https://listmonk.app/static/images/logo.svg";
+          icon = "https://listmonk.app/static/images/favicon.png";
+        }
+      }
+      {
+        "Uptime-Kuma pour le statut des services" = {
+          href = "https://uptime-kuma.resdigita.com";
+          description = "Uptime-Kuma donne une vision d'ensemble sur tous les services des Grands Voisins.";
+          icon = "https://uptime.kuma.pet/img/icon.svg";
+        }
+      }
+    ]
+  };
+  {
+    "Services GV unifiés en alpha" = [
+      {
+        "SilverBullet cahier à plusieurs" = {
+          href = "https://silverbullet.resdigita.com";
+          icon = "https://www.noteapps.ca/content/images/2023/11/SilverBullet---logo.png";
+          description = "SilverBullet permet de rédiger du texte, même à plusieurs, avec une interface fluide pour les documents enregistrés en Markdown dans un système de fichiers et un mécanisme aussi fluide pour le glisser-déposer de fichiers et d'images.";
+        }
+      }
+      {
+        "FileBrowser casier numérique" = {
+          href = "https://filebrowser.resdigita.com/files/";
+          icon = "https://www.gitbook.com/cdn-cgi/image/width=36,dpr=2,height=36,fit=contain,format=auto/https%3A%2F%2F3149836655-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-legacy-files%2Fo%2Fspaces%252F-M8KDxOujDoPpJyJJ5_i%252Favatar-1590579241040.png%3Fgeneration%3D1590579241552005%26alt%3Dmedia";
+          description = "Un espace pour ses fichiers et pour les partager.";
+        }
+      }
+      {
+        "Radicale pour les calendriers partagés" = {
+          href = "https://radicale.resdigita.com";
+          icon = "https://quintagroup.com/cms/python/images/radicale.png/@@images/2bdd655d-2aca-468b-bdd0-81a69ea25658.png";
+          description = "Mettez votre nom d'utisateur sans mot de passe dans l'écran de modification web.";
+        }
+      }
+      {
+        "Xandikos pour un calendrier public partagé" = {
+          href = "https://xandikos.resdigita.com";
+          icon = "https://www.cngcoins.com/photos/big/3960373.jpg";
+          description = "Un calendrier public";
+        }
+      }
+    ]
+  }
+  {
+    "Services GV unifiés dépréciés" = [
+      {
+        "KeeWeb pour la gestion des mots de passe" = {
+          href = "https://keeweb.resdigita.com/";
+          description = "Ce dispostif enregistre les bases de mots de passe dans un dossier secret, permet la syncronisation en plus de l'accès web.";
+          icon = "https://keeweb.info/img/logo.png";
+        }
+      }
+    ]
+  }
+]
