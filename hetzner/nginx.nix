5079265c8ce9b4fa10856b7b808e30f54931727a
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-23 12:09:08 +0100

    Proxy Redirects

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -468,9 +468,9 @@ in
             proxy_set_header  Connection "upgrade";
             # proxy_bind $remote_addr transparent;
             # proxy_set_header Connection $connection_upgrade;
-            proxy_pass http://192.168.108.2; 
+            proxy_pass https://sftpgo.lesgrandsvoisins.com:10443; 
             client_max_body_size 500M;
-            proxy_redirect off;
+            proxy_redirect https://sftpgo.lesgrandsvoisins.com:10443 https://sftpgo.lesgrandsvoisins.com;
             proxy_ssl_verify  off;
             proxy_ssl_trusted_certificate /var/lib/acme/sftp.lesgrandsvoisins.com/fullchain.pem;
           '';
