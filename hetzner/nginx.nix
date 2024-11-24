9ebd04282dadd88d4852c4f82d4149d42324bcd4
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-24 12:13:42 +0100

    Proxy tinkering

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -168,11 +168,11 @@ in
             # proxyPass = "http://localhost:8901";
             extraConfig = ''
             proxy_pass http://localhost:8901;
-            proxy_set_header Host "linkding.lesgrandsvoisins.com";
+            proxy_set_header Host "https://linkding.lesgrandsvoisins.com";
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Host "linkding.lesgrandsvoisins.com";
-            proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_set_header X-Forwarded-Proto "https";
             proxy_redirect off;
             proxy_http_version 1.1;
             proxy_set_header   Upgrade $http_upgrade;
