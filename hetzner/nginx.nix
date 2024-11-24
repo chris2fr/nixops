c48d5a33399d4443cca79bd3c6cf45819e4f9329
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-24 11:57:54 +0100

    Header correction

--- hetzner/nginx.nix
+++ hetzner/nginx.nix
@@ -201,10 +201,10 @@ in
           locations."/" = {
             proxyPass = "https://keycloak.village.ngo:12443";
             extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Host $host;
+            proxy_set_header   X-Real-IP $remote_addr;
+            proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header   Host $host;
+            # proxy_set_header X-Forwarded-Host $host;
             proxy_set_header X-Forwarded-Proto $scheme;
             add_header Content-Security-Policy "frame-src *; frame-ancestors *; object-src *;";
             add_header Access-Control-Allow-Credentials true;
